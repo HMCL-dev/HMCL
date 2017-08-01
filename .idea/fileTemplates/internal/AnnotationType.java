726f92a9c9d7433e7126556796783cdf20a18d0d
huangyuhui <huanghongxun2008@126.com>
2017-08-01 18:12:39 +0800

    Fixed untracked files

--- .idea/fileTemplates/internal/AnnotationType.java
+++
@@ -1,4 +0,0 @@
-#parse("HMCL.java")
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-public @interface ${NAME} {
-}
--- .idea/fileTemplates/internal/Kotlin File.kt
+++
@@ -1,3 +0,0 @@
-#parse("HMCL.java")
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
-#end
