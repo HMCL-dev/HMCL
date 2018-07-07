HMCL-dev/HMCL

commit 3fb5048f103ed958bdd87207f6b6c929b5ef7eca

Fix IllegalStateException in DownloadWizardProvider

HMCL/src/main/java/org/jackhuang/hmcl/ui/animation/TransitionHandler.java
@@ -80,7 +80,10 @@ public final class TransitionHandler implements AnimationHandler {
 
         Timeline nowAnimation = new Timeline();
         nowAnimation.getKeyFrames().addAll(transition.animate(this));
-        nowAnimation.getKeyFrames().add(new KeyFrame(duration, e -> view.getChildren().remove(previousNode)));
+        nowAnimation.getKeyFrames().add(new KeyFrame(duration, e -> {
+            previousNode.setMouseTransparent((Boolean) previousNode.getProperties().get(MOUSE_TRANSPARENT));
+            view.getChildren().remove(previousNode);
+        }));
         nowAnimation.play();
         animation = nowAnimation;
     }
@@ -96,10 +99,14 @@ public final class TransitionHandler implements AnimationHandler {
         if (previousNode == newView)
             previousNode = NULL;
 
+        previousNode.getProperties().put(MOUSE_TRANSPARENT, previousNode.isMouseTransparent());
+        previousNode.setMouseTransparent(true);
+
         currentNode = newView;
 
         view.getChildren().setAll(previousNode, currentNode);
     }
 
+    private static final String MOUSE_TRANSPARENT = "TransitionHandler.MOUSE_TRANSPARENT";
     private static final StackPane NULL = new StackPane();
 }

commit 87904109ca7e2557ebe8c28c55dc9d6baeca924b

[TransitionHandler]Set mouseTransparent of the whole view , fix bug introduced in 3fb5048f103ed958bdd87207f6b6c929b5ef7eca

HMCL/src/main/java/org/jackhuang/hmcl/ui/animation/TransitionHandler.java
@@ -86,7 +86,7 @@ public final class TransitionHandler implements AnimationHandler {
             Timeline nowAnimation = new Timeline();
             nowAnimation.getKeyFrames().addAll(transition.animate(this));
             nowAnimation.getKeyFrames().add(new KeyFrame(duration, e -> {
-                previousNode.setMouseTransparent((Boolean) previousNode.getProperties().get(MOUSE_TRANSPARENT));
+                view.setMouseTransparent(false);
                 view.getChildren().remove(previousNode);
             }));
             nowAnimation.play();
@@ -105,14 +105,12 @@ public final class TransitionHandler implements AnimationHandler {
         if (previousNode == newView)
             previousNode = NULL;
 
-        previousNode.getProperties().put(MOUSE_TRANSPARENT, previousNode.isMouseTransparent());
-        previousNode.setMouseTransparent(true);
+        view.setMouseTransparent(true);
 
         currentNode = newView;
 
         view.getChildren().setAll(previousNode, currentNode);
     }
 
-    private static final String MOUSE_TRANSPARENT = "TransitionHandler.MOUSE_TRANSPARENT";
     private static final StackPane NULL = new StackPane();
 }