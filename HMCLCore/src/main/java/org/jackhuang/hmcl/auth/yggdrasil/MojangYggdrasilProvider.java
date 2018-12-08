672b6054a6cb75cebfe13120625ff7db0dbaa6a0
yushijinhun <yushijinhun@gmail.com>
2018-12-08 21:13:03 +0800

    Format license headers

--- HMCLCore/src/main/java/org/jackhuang/hmcl/auth/yggdrasil/MojangYggdrasilProvider.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/auth/yggdrasil/MojangYggdrasilProvider.java
@@ -1,3 +1,20 @@
+/**
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2018  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.auth.yggdrasil;
 
 import org.jackhuang.hmcl.util.gson.UUIDTypeAdapter;
--- HMCLCore/src/main/java/org/jackhuang/hmcl/game/StringArgument.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/game/StringArgument.java
@@ -1,7 +1,7 @@
-/*
- * Hello Minecraft! Launcher.
- * Copyright (C) 2013  huangyuhui <huanghongxun2008@126.com>
- * 
+/**
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2018  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see {http://www.gnu.org/licenses/}.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package org.jackhuang.hmcl.game;
 
