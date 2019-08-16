520ffe4c093479d495d3c42f111bf7a9b7e40f30
huanghongxun <huanghongxun2008@126.com>
2019-08-17 00:45:47 +0800

    Add missing license header

--- HMCLCore/src/test/java/org/jackhuang/hmcl/util/TaskTest.java
+++ HMCLCore/src/test/java/org/jackhuang/hmcl/util/TaskTest.java
@@ -1,3 +1,20 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2019  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.util;
 
 import javafx.embed.swing.JFXPanel;
@@ -89,7 +106,6 @@ public class TaskTest {
         Assert.assertNull("CancellationException should not be recorded.", task.getException());
     }
 
-    @Test
     public void testRejectedExecutionException() {
         Schedulers.defaultScheduler();
         Schedulers.shutdown();
