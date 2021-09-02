c4c78db8fd4aa2c18232e87b688cd59577ed95af
Glavo <zjx001202@126.com>
2021-09-02 23:26:09 +0800

    fix style

--- HMCL/src/main/java/org/jackhuang/hmcl/util/SelfDependencyPatcher.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/util/SelfDependencyPatcher.java
@@ -368,10 +368,10 @@ public final class SelfDependencyPatcher {
             setLocationRelativeTo(null);
 
             GridBagLayout gridBagLayout = new GridBagLayout();
-            gridBagLayout.columnWidths = new int[]{600, 0};
-            gridBagLayout.rowHeights = new int[]{0, 0, 0, 200};
-            gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-            gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0};
+            gridBagLayout.columnWidths = new int[] { 600, 0 };
+            gridBagLayout.rowHeights = new int[] { 0, 0, 0, 200 };
+            gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+            gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0 };
             panel.setLayout(gridBagLayout);
 
             progressText = new JLabel("");
