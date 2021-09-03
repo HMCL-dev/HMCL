faede1666250ca5dbf6550aaadc70412c65eb78d
Glavo <zjx001202@126.com>
2021-09-03 14:33:13 +0800

    fix: LZMAInputStream not closed

--- HMCLCore/src/main/java/org/jackhuang/hmcl/download/java/JavaDownloadTask.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/java/JavaDownloadTask.java
@@ -98,8 +98,8 @@ public class JavaDownloadTask extends Task<Void> {
                     FileDownloadTask task = new FileDownloadTask(NetworkUtils.toURL(download.getUrl()), tempFile, new FileDownloadTask.IntegrityCheck("SHA-1", download.getSha1()));
                     task.setName(entry.getKey());
                     dependencies.add(task.thenRunAsync(() -> {
-                        try {
-                            Files.copy(new LZMAInputStream(new FileInputStream(tempFile)), dest);
+                        try (LZMAInputStream input = new LZMAInputStream(new FileInputStream(tempFile))) {
+                            Files.copy(input, dest);
                         } catch (IOException e) {
                             throw new ArtifactMalformedException("File " + entry.getKey() + " is malformed");
                         }
