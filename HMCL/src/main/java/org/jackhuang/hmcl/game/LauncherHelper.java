464cd345c92c3bbe1666eb8dd20e7287ccb8e75b
Glavo <zjx001202@gmail.com>
2021-12-11 14:30:54 +0800

    Use custom log4j2.xml

--- HMCL/src/main/java/org/jackhuang/hmcl/game/LauncherHelper.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/game/LauncherHelper.java
@@ -72,11 +72,6 @@ import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
 
 public final class LauncherHelper {
 
-    private static final Set<String> UNSAFE_CLIENT_1_7_XML_SHA1 =
-            Collections.unmodifiableSet(new HashSet<>(Collections.singletonList("6605d632a2399010c0085d3e4da58974d62ccdfe")));
-    private static final Set<String> UNSAFE_CLIENT_1_12_XML_SHA1 =
-            Collections.unmodifiableSet(new HashSet<>(Collections.singletonList("ef4f57b922df243d0cef096efe808c72db042149")));
-
     private final Profile profile;
     private final Account account;
     private final String selectedVersion;
@@ -523,31 +518,12 @@ public final class LauncherHelper {
                                 VersionNumber.VERSION_COMPARATOR.compare("1.14.4-28.2.2", it.getVersion()) <= 0);
                 boolean hasOptiFine = version.getLibraries().stream().anyMatch(it -> it.is("optifine", "OptiFine"));
                 if (hasForge28_2_2 && hasOptiFine && gameVersion.compareTo(VersionNumber.asVersion("1.14.4")) == 0) {
-                    Controllers.confirm(i18n("launch.advice.forge28_2_2_optifine"), i18n("message.error"), continueAction, null);
+                    Controllers.confirm(i18n("launch.advice.forge28_2_2_optifine"), i18n("message.error"), continueAction, breakAction);
                     suggested = true;
                 }
             }
 
-            // CVE-2021-44228 Remote code injection in Log4j
-            if (gameVersion.compareTo(VersionNumber.asVersion("1.7")) >= 0 && gameVersion.compareTo(VersionNumber.asVersion("1.18")) <= 0) {
-                String xmlSha1 = Optional.ofNullable(version.getLogging().get(DownloadType.CLIENT))
-                        .flatMap(loggingInfo -> Optional.of(loggingInfo.getFile()))
-                        .flatMap(idDownloadInfo -> Optional.ofNullable(idDownloadInfo.getSha1()))
-                        .orElse("");
-                if (gameVersion.compareTo(VersionNumber.asVersion("1.12")) < 0) {
-                    if (UNSAFE_CLIENT_1_7_XML_SHA1.contains(xmlSha1)) {
-                        Controllers.confirm(i18n("launch.advice.log4j_cve_2021_44228"), i18n("message.warning"), continueAction, breakAction);
-                        suggested = true;
-                    }
-                } else {
-                    if (UNSAFE_CLIENT_1_12_XML_SHA1.contains(xmlSha1)) {
-                        Controllers.confirm(i18n("launch.advice.log4j_cve_2021_44228"), i18n("message.warning"), continueAction, breakAction);
-                        suggested = true;
-                    }
-                }
-            }
-
-            if (!suggested) {
+            if (!future.isDone()) {
                 future.complete(javaVersion);
             }
 
