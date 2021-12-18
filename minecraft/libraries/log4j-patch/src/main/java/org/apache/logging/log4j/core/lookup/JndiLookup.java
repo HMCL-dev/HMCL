c657a1d2cca5839ed803c4ed854079653dae6b63
Glavo <zjx001202@gmail.com>
2021-12-18 10:38:05 +0800

    Introducing log4j-patch

---
+++ minecraft/libraries/log4j-patch/src/main/java/org/apache/logging/log4j/core/lookup/JndiLookup.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2021 Glavo <zjx001202@gmail.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package org.apache.logging.log4j.core.lookup;
+
+public class JndiLookup {
+    public JndiLookup() {
+        throw new NoClassDefFoundError("JNDI lookup is disabled");
+    }
+}
