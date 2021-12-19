4f1af87ba0f5f86ae24a1c7ab1f757455280c013
huanghongxun <i@huangyuhui.net>
2021-12-19 20:31:05 +0800

    fix(download): 1.18 released.

--- HMCLCore/src/main/java/org/jackhuang/hmcl/mod/RemoteModRepository.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/mod/RemoteModRepository.java
@@ -61,6 +61,7 @@ public interface RemoteModRepository {
     }
 
     String[] DEFAULT_GAME_VERSIONS = new String[]{
+            "1.18.1", "1.18",
             "1.17.1", "1.17",
             "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16",
             "1.15.2", "1.15.1", "1.15",
