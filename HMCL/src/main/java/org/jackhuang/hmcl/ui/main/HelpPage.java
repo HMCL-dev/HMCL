fc2393a52f8f13174521305971be6e1772d702a8
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-03-21 21:57:05 +0800

    Update HelpPage.java

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/HelpPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/HelpPage.java
@@ -50,8 +50,8 @@ public class HelpPage extends SpinnerPane {
         JFXScrollPane.smoothScrolling(scrollPane);
         setContent(scrollPane);
 
+        IconedTwoLineListItem docPane = new IconedTwoLineListItem();
         ComponentList doc = new ComponentList();
-        doc.getContent().setAll(docPane);
         content.getChildren().add(doc);
 
         loadHelp();
