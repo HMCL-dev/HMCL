cdff3450d4b96e27f115605de144723dd8945e7c
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-04-04 01:39:24 +0800

    Update SponsorPage.java

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/SponsorPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/SponsorPage.java
@@ -109,7 +109,7 @@ public class SponsorPage extends StackPane {
     }
 
     private void loadSponsorList() {
-        Task.<List<Sponsor>>supplyAsync(() -> HttpRequest.GET("https://cdn.jsdelivr.net/gh/huanghongxun/HMCL/sponsor/sponsor.json").getJson(new TypeToken<List<Sponsor>>() {
+        Task.<List<Sponsor>>supplyAsync(() -> HttpRequest.GET("https://cdn.jsdelivr.net/gh/zkitefly/myphoto/Nsponsor.json").getJson(new TypeToken<List<Sponsor>>() {
         }.getType())).thenAcceptAsync(Schedulers.javafx(), sponsors -> {
             listView.getItems().setAll(sponsors);
         }).start();
