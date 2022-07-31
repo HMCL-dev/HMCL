66427897a014b0eaf7804da11f8085fdcce5fcd4
Glavo <zjx001202@gmail.com>
2022-07-31 20:13:47 +0800

    Add support for Linux ARM

--- HMCL/src/main/java/org/jackhuang/hmcl/util/NativePatcher.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/util/NativePatcher.java
@@ -1,18 +1,21 @@
 package org.jackhuang.hmcl.util;
 
+import com.google.gson.reflect.TypeToken;
 import org.jackhuang.hmcl.game.Library;
 import org.jackhuang.hmcl.game.NativesDirectoryType;
 import org.jackhuang.hmcl.game.Version;
 import org.jackhuang.hmcl.setting.VersionSetting;
+import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.platform.Architecture;
 import org.jackhuang.hmcl.util.platform.JavaVersion;
 import org.jackhuang.hmcl.util.platform.OperatingSystem;
-import org.jackhuang.hmcl.util.platform.Platform;
 import org.jackhuang.hmcl.util.versioning.VersionNumber;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.*;
+import java.util.logging.Level;
 
 import static org.jackhuang.hmcl.util.Logging.LOG;
 
@@ -32,7 +35,7 @@ public final class NativePatcher {
         if (settings.getNativesDirType() != NativesDirectoryType.VERSION_FOLDER)
             return version;
 
-        Map<String, Library> replacements = Hole.nativeReplacement.get(javaVersion.getPlatform());
+        Map<String, Library> replacements = Hole.nativeReplacement.get(javaVersion.getPlatform().toString());
         if (replacements == null) {
             LOG.warning("No alternative native library provided for platform " + javaVersion.getPlatform());
             return version;
@@ -41,22 +44,23 @@ public final class NativePatcher {
         ArrayList<Library> newLibraries = new ArrayList<>();
         for (Library library : version.getLibraries()) {
             if (!library.appliesToCurrentEnvironment()) {
-                newLibraries.add(library);
                 continue;
             }
 
-            Library replacement = replacements.get(library.getName());
-            if (replacement == null) {
-                if (!library.isNative()
-                        || (settings.isUseNativeGLFW() && library.getArtifactId().contains("glfw"))
-                        || (settings.isUseNativeOpenAL() && library.getArtifactId().contains("openal"))) {
+            if (library.isNative()) {
+                Library replacement = replacements.get(library.getName() + ":natives");
+                if (replacement == null) {
+                    if (!(settings.isUseNativeGLFW() && library.getArtifactId().contains("glfw"))
+                            && !(settings.isUseNativeOpenAL() && library.getArtifactId().contains("openal"))) {
+                        LOG.warning("No alternative native library " + library.getName() + "provided for platform " + javaVersion.getPlatform());
+                        return version;
+                    }
                     newLibraries.add(library);
                 } else {
-                    LOG.warning("No alternative native library " + library.getName() + "provided for platform " + javaVersion.getPlatform());
-                    return version;
+                    newLibraries.add(replacement);
                 }
             } else {
-                newLibraries.add(replacement);
+                newLibraries.add(replacements.getOrDefault(library.getName(), library));
             }
         }
 
@@ -64,6 +68,17 @@ public final class NativePatcher {
     }
 
     private static final class Hole {
-        static final Map<Platform, Map<String, Library>> nativeReplacement = new HashMap<>(); // TODO
+        static Map<String, Map<String, Library>> nativeReplacement;
+
+        static {
+            //noinspection ConstantConditions
+            try (Reader reader = new InputStreamReader(NativePatcher.class.getResourceAsStream("/assets/natives.json"))) {
+                nativeReplacement = JsonUtils.GSON.fromJson(reader, new TypeToken<Map<String, Map<String, Library>>>() {
+                }.getType());
+            } catch (IOException e) {
+                nativeReplacement = Collections.emptyMap();
+                LOG.log(Level.WARNING, "Failed to load native library list", e);
+            }
+        }
     }
 }
