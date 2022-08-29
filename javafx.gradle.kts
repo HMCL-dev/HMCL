27df2e1a27e6f03da8d4d0c7debb7204b960b529
Glavo <zjx001202@gmail.com>
2022-08-29 13:18:27 +0800

    Automatically download JavaFX on linux-loongarch64_ow

--- javafx.gradle.kts
+++ javafx.gradle.kts
@@ -39,6 +39,7 @@ val jfxPlatforms = listOf(
     Platform("linux-x86_64", "linux"),
     Platform("linux-arm32", "linux-arm32-monocle", unsupportedModules = listOf("media", "web")),
     Platform("linux-arm64", "linux-aarch64"),
+    Platform("linux-loongarch64_ow", "linux", groupId = "org.glavo.hmcl.openjfx", version = "19-ea+10-loongson64"),
 )
 
 val jfxInClasspath =
