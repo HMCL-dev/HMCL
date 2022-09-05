7ddcae0104c20077ded8ebe668c43821d47e05ba
Chikage <chikage@939.me>
2022-09-05 18:44:28 +0800

    fix modrinth modpack install

--- HMCLCore/src/main/java/org/jackhuang/hmcl/mod/modrinth/ModrinthCompletionTask.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/mod/modrinth/ModrinthCompletionTask.java
@@ -102,6 +102,8 @@ public class ModrinthCompletionTask extends Task<Void> {
         Path runDirectory = repository.getRunDirectory(version).toPath();
 
         for (ModrinthManifest.File file : manifest.getFiles()) {
+            if (file.getEnv() != null && file.getEnv().getOrDefault("client", null).equals("unsupported"))
+                continue;
             Path filePath = runDirectory.resolve(file.getPath());
             if (!Files.exists(filePath) && !file.getDownloads().isEmpty()) {
                 FileDownloadTask task = new FileDownloadTask(file.getDownloads().get(0), filePath.toFile());
