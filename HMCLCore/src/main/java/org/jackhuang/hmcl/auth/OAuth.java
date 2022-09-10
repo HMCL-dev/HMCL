d2b77c4a1c946ef78f5a2b3539a14f7831769e23
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-09-10 12:37:18 +0800

    再添加一个qwq

--- HMCLCore/src/main/java/org/jackhuang/hmcl/auth/OAuth.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/auth/OAuth.java
@@ -184,6 +184,7 @@ public class OAuth {
                 if (response.errorDescription.contains("The user must sign in again and if needed grant the client application access to the requested scope") ||
                         response.errorDescription.contains("The user must first sign in and grant the client application access to the requested scope") ||
                         response.errorDescription.contains("The user must sign in again") ||
+                        response.errorDescription.contains("The provided value for the input parameter 'refresh_token' or 'assertion' is not valid") ||
                         response.errorDescription.contains("The user could not be authenticated as the grant is expired")) {
                     throw new CredentialExpiredException();
                 }
