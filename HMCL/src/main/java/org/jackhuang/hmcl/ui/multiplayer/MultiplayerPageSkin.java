df31ece571df38df9cf5cec161651ec2ef7859bb
zkitefly <64117916+zkitefly@users.noreply.github.com>
2022-10-01 23:52:56 +0800

    fix

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/multiplayer/MultiplayerPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/multiplayer/MultiplayerPageSkin.java
@@ -295,9 +295,9 @@ public class MultiplayerPageSkin extends DecoratorAnimatedPage.DecoratorAnimated
 
                 FXUtils.onChangeAndOperate(getSkinnable().sessionProperty(), session -> {
                     if (session == null) {
-                        mainPane.getChildren().setAll(new Label("HiPer"),);
+                        mainPane.getChildren().setAll(new Label("HiPer"));
                     } else {
-                        mainPane.getChildren().setAll(new Label("HiPer"),);
+                        mainPane.getChildren().setAll(new Label("HiPer"));
                     }
                 });
             }
