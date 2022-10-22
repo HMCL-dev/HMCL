b2b9d30cda544a8a2e048f32b06cf7520cc31e4b
zkitefly <z18344203426@qq.com>
2022-10-22 15:02:21 +0800

    再次尝试修复多人联机UI

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/multiplayer/MultiplayerPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/multiplayer/MultiplayerPageSkin.java
@@ -200,12 +200,11 @@ public class MultiplayerPageSkin extends DecoratorAnimatedPage.DecoratorAnimated
                         HintPane hintPane1 = new HintPane(MessageDialogPane.MessageType.INFO);
                         GridPane.setColumnSpan(hintPane1, 3);
                         hintPane1.setText(i18n("multiplayer.master.hint"));
-                        masterPane.addRow(hintPane1);
-
+                        
                         HintPane hintPane2 = new HintPane(MessageDialogPane.MessageType.INFO);
                         GridPane.setColumnSpan(hintPane2, 3);
                         hintPane2.setText(i18n("multiplayer.master.hint2"));
-                        masterPane.addRow(hintPane2);
+                        masterPane.addRow(0, hintPane1, hintPane2);
 
                         Label portTitle = new Label(i18n("multiplayer.master.port"));
                         BorderPane.setAlignment(portTitle, Pos.CENTER_LEFT);
