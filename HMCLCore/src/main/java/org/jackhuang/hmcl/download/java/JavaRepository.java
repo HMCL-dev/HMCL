9abcb534b32de1c6c79b16bf24bba6878ba58998
Glavo <zjx001202@gmail.com>
2022-11-26 16:51:15 +0800

    Verify Java at startup

--- HMCLCore/src/main/java/org/jackhuang/hmcl/download/java/JavaRepository.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/java/JavaRepository.java
@@ -9,10 +9,9 @@ import org.jackhuang.hmcl.util.platform.Architecture;
 import org.jackhuang.hmcl.util.platform.JavaVersion;
 import org.jackhuang.hmcl.util.platform.OperatingSystem;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -72,23 +71,53 @@ public final class JavaRepository {
         // $HOME/.minecraft/runtime/java-runtime-beta/linux/java-runtime-beta
         Optional<String> platformOptional = getSystemJavaPlatform();
         if (!platformOptional.isPresent()) return Stream.empty();
-        String platform = platformOptional.get();
         List<Path> javaHomes = new ArrayList<>();
         try (DirectoryStream<Path> dir = Files.newDirectoryStream(runtimeDir)) {
             // component can be jre-legacy, java-runtime-alpha, java-runtime-beta, java-runtime-gamma or any other being added in the future.
             for (Path component : dir) {
-                if (Files.isRegularFile(component.resolve(platform).resolve(component.getFileName() + ".sha1"))) {
-                    Path javaHome = component.resolve(platform).resolve(component.getFileName());
-                    if (OperatingSystem.CURRENT_OS == OperatingSystem.OSX) {
-                        javaHome = javaHome.resolve("jre.bundle/Contents/Home");
-                    }
-                    javaHomes.add(javaHome);
-                }
+                findJavaHomeInComponentDir(platformOptional.get(), component).ifPresent(javaHomes::add);
             }
         } catch (IOException e) {
             LOG.log(Level.WARNING, "Failed to list java-runtime directory " + runtimeDir, e);
         }
         return javaHomes.stream();
+    }
+
+    private static Optional<Path> findJavaHomeInComponentDir(String platform, Path component) {
+        Path sha1File = component.resolve(platform).resolve(component.getFileName() + ".sha1");
+        if (!Files.isRegularFile(sha1File))
+            return Optional.empty();
+        Path dir = component.resolve(platform).resolve(component.getFileName());
+
+        try (BufferedReader reader = Files.newBufferedReader(sha1File)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) continue;
+
+                int idx = line.indexOf(" /#//");
+                if (idx <= 0)
+                    throw new IOException("Illegal line: " + line);
+
+                Path file = dir.resolve(line.substring(0, idx));
+
+                // Should we check the sha1 of files? This will take a lot of time.
+                if (Files.notExists(file))
+                    throw new NoSuchFileException(file.toAbsolutePath().toString());
+            }
+
+            if (OperatingSystem.CURRENT_OS == OperatingSystem.OSX) {
+                Path macPath = dir.resolve("jre.bundle/Contents/Home");
+                if (Files.exists(macPath))
+                    return Optional.of(macPath);
+                else
+                    LOG.warning("The Java is not in 'jre.bundle/Contents/Home'");
+            }
+
+            return Optional.of(dir);
+        } catch (IOException e) {
+            LOG.log(Level.WARNING, "Failed to verify Java in " + component, e);
+            return Optional.empty();
+        }
     }
 
     public static Optional<String> getSystemJavaPlatform() {
