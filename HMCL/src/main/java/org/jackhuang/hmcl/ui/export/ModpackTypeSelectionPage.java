9ba2575eb4177d09bfcb860dab2e0cada9e7d712
Glavo <zjx001202@gmail.com>
2022-12-10 06:44:52 +0800

    Remove type.fxml

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/export/ModpackTypeSelectionPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/export/ModpackTypeSelectionPage.java
@@ -18,46 +18,65 @@
 package org.jackhuang.hmcl.ui.export;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import javafx.scene.layout.StackPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.SVGPath;
 import org.jackhuang.hmcl.mod.ModpackExportInfo;
 import org.jackhuang.hmcl.mod.mcbbs.McbbsModpackExportTask;
 import org.jackhuang.hmcl.mod.multimc.MultiMCModpackExportTask;
 import org.jackhuang.hmcl.mod.server.ServerModpackExportTask;
-import org.jackhuang.hmcl.ui.FXUtils;
+import org.jackhuang.hmcl.ui.SVG;
+import org.jackhuang.hmcl.ui.construct.TwoLineListItem;
 import org.jackhuang.hmcl.ui.wizard.WizardController;
 import org.jackhuang.hmcl.ui.wizard.WizardPage;
+import org.jackhuang.hmcl.util.Lang;
 
 import java.util.Map;
 
 import static org.jackhuang.hmcl.ui.export.ModpackInfoPage.MODPACK_INFO_OPTION;
 import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
 
-public final class ModpackTypeSelectionPage extends StackPane implements WizardPage {
+public final class ModpackTypeSelectionPage extends VBox implements WizardPage {
     private final WizardController controller;
-    @FXML
-    private JFXButton btnMCBBS;
-    @FXML
-    private JFXButton btnMultiMC;
-    @FXML
-    private JFXButton btnServer;
 
     public ModpackTypeSelectionPage(WizardController controller) {
         this.controller = controller;
-        FXUtils.loadFXML(this, "/assets/fxml/modpack/type.fxml");
 
-        JFXButton[] buttons = new JFXButton[]{btnMCBBS, btnMultiMC, btnServer};
-        String[] types = new String[]{MODPACK_TYPE_MCBBS, MODPACK_TYPE_MULTIMC, MODPACK_TYPE_SERVER};
-        ModpackExportInfo.Options[] options = new ModpackExportInfo.Options[]{McbbsModpackExportTask.OPTION, MultiMCModpackExportTask.OPTION, ServerModpackExportTask.OPTION};
-        for (int i = 0; i < types.length; ++i) {
-            String type = types[i];
-            ModpackExportInfo.Options option = options[i];
-            buttons[i].setOnMouseClicked(e -> {
-                controller.getSettings().put(MODPACK_TYPE, type);
-                controller.getSettings().put(MODPACK_INFO_OPTION, option);
-                controller.onNext();
-            });
-        }
+        this.getStyleClass().add("jfx-list-view");
+        this.setMaxSize(300, 150);
+        this.getChildren().setAll(
+                Lang.apply(new Label(i18n("modpack.export.as")), title -> title.setPadding(new Insets(8))),
+                createButton("modpack.type.mcbbs", "modpack.type.mcbbs.export", MODPACK_TYPE_MCBBS, McbbsModpackExportTask.OPTION),
+                createButton("modpack.type.multimc", "modpack.type.multimc.export", MODPACK_TYPE_MULTIMC, MultiMCModpackExportTask.OPTION),
+                createButton("modpack.type.server", "modpack.type.server.export", MODPACK_TYPE_SERVER, ServerModpackExportTask.OPTION)
+        );
+    }
+
+    private JFXButton createButton(String titleKey, String detailKey, String type, ModpackExportInfo.Options option) {
+        JFXButton button = new JFXButton();
+        button.setOnAction(e -> {
+            controller.getSettings().put(MODPACK_TYPE, type);
+            controller.getSettings().put(MODPACK_INFO_OPTION, option);
+            controller.onNext();
+        });
+
+        button.prefWidthProperty().bind(this.widthProperty());
+
+        BorderPane graphic = new BorderPane();
+        graphic.setMouseTransparent(true);
+        graphic.setLeft(new TwoLineListItem(i18n(titleKey), i18n(detailKey)));
+
+        SVGPath arrow = new SVGPath();
+        arrow.setContent(SVG.ARROW_RIGHT);
+        BorderPane.setAlignment(arrow, Pos.CENTER);
+        graphic.setRight(arrow);
+
+        button.setGraphic(graphic);
+
+        return button;
     }
 
     @Override
