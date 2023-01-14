95efb70f32bdda4da84137b2d022c712a731484a
Glavo <zjx001202@gmail.com>
2023-01-15 07:41:22 +0800

    Migrate NormalizedSkin to JavaFX

--- HMCLCore/src/test/java/org/jackhuang/hmcl/util/skin/NormalizedSkinTest.java
+++ HMCLCore/src/test/java/org/jackhuang/hmcl/util/skin/NormalizedSkinTest.java
@@ -1,21 +1,22 @@
 package org.jackhuang.hmcl.util.skin;
 
+import javafx.scene.image.Image;
+import org.jackhuang.hmcl.JavaFXLauncher;
 import org.junit.jupiter.api.Test;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NormalizedSkinTest {
-    private static NormalizedSkin getSkin(String name) throws IOException, InvalidSkinException {
-        File path = new File("../HMCL/src/main/resources/assets/img/skin/" + name + ".png");
-        return new NormalizedSkin(ImageIO.read(path));
+    private static NormalizedSkin getSkin(String name) throws InvalidSkinException {
+        String path = Paths.get("../HMCL/src/main/resources/assets/img/skin/" + name + ".png").normalize().toAbsolutePath().toUri().toString();
+        return new NormalizedSkin(new Image(path));
     }
 
     @Test
     public void testIsSlim() throws Exception {
+        JavaFXLauncher.start();
         assertFalse(getSkin("steve").isSlim());
         assertTrue(getSkin("alex").isSlim());
         assertTrue(getSkin("noor").isSlim());
