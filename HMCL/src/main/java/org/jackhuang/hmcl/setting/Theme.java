2cce24822f9755c4da4ea8f8a262de1ca1007eec
Glavo <zjx001202@gmail.com>
2023-01-22 22:38:57 +0800

    Check JavaFX Version

--- HMCL/src/main/java/org/jackhuang/hmcl/setting/Theme.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/setting/Theme.java
@@ -23,6 +23,7 @@ import com.google.gson.stream.JsonWriter;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.ObjectBinding;
 import javafx.scene.paint.Color;
+import org.jackhuang.hmcl.util.Lang;
 import org.jackhuang.hmcl.util.Logging;
 import org.jackhuang.hmcl.util.io.FileUtils;
 import org.jackhuang.hmcl.util.io.IOUtils;
@@ -30,10 +31,13 @@ import org.jackhuang.hmcl.util.io.IOUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.jackhuang.hmcl.setting.ConfigHolder.config;
 
@@ -49,6 +53,32 @@ public class Theme {
             Color.web("#9C27B0"), // purple
             Color.web("#B71C1C")  // red
     };
+
+    private static Charset cssCharset;
+
+    private static Charset getCssCharset() {
+        if (cssCharset != null)
+            return cssCharset;
+
+        Charset defaultCharset = Charset.defaultCharset();
+        if (defaultCharset != StandardCharsets.UTF_8) {
+            String jfxVersion = System.getProperty("javafx.version");
+            if (jfxVersion != null) {
+                Matcher matcher = Pattern.compile("^(?<version>[0-9]+)").matcher(jfxVersion);
+                if (matcher.find()) {
+                    int v = Lang.parseInt(matcher.group(), -1);
+                    if (v >= 18) {
+                        cssCharset = StandardCharsets.UTF_8;
+                    }
+                }
+            }
+        }
+
+        if (cssCharset == null)
+            cssCharset = defaultCharset;
+
+        return cssCharset;
+    }
 
     public static Theme getTheme() {
         Theme theme = config().getTheme();
@@ -103,7 +133,7 @@ public class Theme {
                         .replace("%disabled-font-color%", String.format("rgba(%d, %d, %d, 0.7)", (int) Math.ceil(textFill.getRed() * 256), (int) Math.ceil(textFill.getGreen() * 256), (int) Math.ceil(textFill.getBlue() * 256)))
                         .replace("%font-color%", getColorDisplayName(getForegroundColor()))
                         .replace("%font%", Optional.ofNullable(fontFamily).map(f -> "-fx-font-family: \"" + f + "\";").orElse(""));
-                FileUtils.writeText(temp, themeText, Charset.defaultCharset());
+                FileUtils.writeText(temp, themeText, getCssCharset());
                 temp.deleteOnExit();
                 css = temp.toURI().toString();
             } catch (IOException | NullPointerException e) {
