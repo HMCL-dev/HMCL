21233578972074c02e1dd976b75576583592e04a
2573874409@qq.com <z18344203426@qq.com>
2023-01-30 13:34:13 +0800

    将初始化内容设定为“名称”排序

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadListPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadListPage.java
@@ -118,7 +118,7 @@ public class DownloadListPage extends Control implements DecoratorPage, VersionP
 
         if (!searchInitialized) {
             searchInitialized = true;
-            search("", null, 0, "", RemoteModRepository.SortType.DATE_CREATED);
+            search("", null, 0, "", RemoteModRepository.SortType.NAME);
         }
 
         if (versionSelection) {
