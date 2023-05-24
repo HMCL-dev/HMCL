c320a05feafb8edbe7d19bfbc8d5d0a971b85e71
Glavo <zjx001202@gmail.com>
2023-05-24 22:02:45 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/VersionSettingsPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/VersionSettingsPage.java
@@ -472,6 +472,7 @@ public final class VersionSettingsPage extends StackPane implements DecoratorPag
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void loadVersion(Profile profile, String versionId) {
         this.profile = profile;
         this.versionId = versionId;
@@ -555,9 +556,7 @@ public final class VersionSettingsPage extends StackPane implements DecoratorPag
             } else if (javaAutoDeterminedOption.isSelected()) {
                 versionSetting.setJavaAutoSelected();
             } else {
-                @SuppressWarnings("unchecked")
-                JavaVersion version = ((Pair<JavaVersionType, JavaVersion>) newValue.getUserData()).getValue();
-                versionSetting.setJavaVersion(version);
+                versionSetting.setJavaVersion(((Pair<JavaVersionType, JavaVersion>) newValue.getUserData()).getValue());
             }
         });
 
