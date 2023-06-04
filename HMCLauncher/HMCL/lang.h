df59951635adf50f5f3f1a1e780ec1f4968207c3
2573874409@qq.com <z18344203426@qq.com>
2023-06-04 09:03:10 +0800

    更新 lang.h

--- HMCLauncher/HMCL/lang.h
+++ HMCLauncher/HMCL/lang.h
@@ -2,19 +2,13 @@
 
 #define ERROR_TITLE L"Java not found"
 
-#define ERROR_TITLE_ZH L"æœªæ‰¾åˆ° Java"
+#define ERROR_TITLE_ZH L"æœ?æ‰¾åˆ° Java"
 
 #define ERROR_PROMPT L"The Java runtime environment is required to run HMCL and Minecraft,\n"\
                      L"Click 'OK' to start downloading java.\n"\
                      L"Please restart HMCL after installing Java.\n"\
                      L"Click 'Help' go for help."
 
-<<<<<<< Updated upstream
-#define ERROR_PROMPT_ZH L"è¿è¡Œ HMCL ä»¥åŠ Minecraft éœ€è¦ Java è¿è¡Œæ—¶ç¯å¢ƒï¼Œç‚¹å‡»â€œç¡®å®šâ€å¼€å§‹ä¸‹è½½ã€‚\n"\
-                        L"è¯·åœ¨å®‰è£… Java å®Œæˆåé‡æ–°å¯åŠ¨ HMCLã€‚\n"\
-                        L"ç‚¹å‡»â€œå¸®åŠ©â€å¯»æ±‚å¸®åŠ©ã€‚"
-=======
-#define ERROR_PROMPT_ZH L"ÔËĞĞ HMCL ÒÔ¼° Minecraft ĞèÒª Java ÔËĞĞÊ±»·¾³£¬µã»÷¡°È·¶¨¡±¿ªÊ¼ÏÂÔØ¡£\n"\
-                        L"ÇëÔÚ°²×° Java Íê³ÉºóÖØĞÂÆô¶¯ HMCL¡£\n"\
-                        L"µã»÷¡°°ïÖú¡±Ñ°Çó°ïÖú¡£"
->>>>>>> Stashed changes
+#define ERROR_PROMPT_ZH L"è¿è?? HMCL ä»¥åŠ Minecraft éœ€è¦? Java è¿è?Œæ—¶ç?å¢ƒï¼Œç‚¹å‡»â€œç¡®å®šâ€å¼€å§‹ä¸‹è½½ã€‚\n"\
+                        L"è¯·åœ¨å®‰è?? Java å®Œæˆåé‡æ–°å¯åŠ? HMCLã€‚\n"\
+                        L"ç‚¹å‡»â€œå¸®åŠ©â€å?»æ±‚å¸?åŠ©ã€?"
