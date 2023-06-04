d1bd46b63552cb0afdc8e98ff797598cb9679ffe
2573874409@qq.com <z18344203426@qq.com>
2023-06-04 09:02:41 +0800

    更新 lang.h

--- HMCLauncher/HMCL/lang.h
+++ HMCLauncher/HMCL/lang.h
@@ -9,6 +9,12 @@
                      L"Please restart HMCL after installing Java.\n"\
                      L"Click 'Help' go for help."
 
+<<<<<<< Updated upstream
 #define ERROR_PROMPT_ZH L"è¿è¡Œ HMCL ä»¥åŠ Minecraft éœ€è¦ Java è¿è¡Œæ—¶çŽ¯å¢ƒï¼Œç‚¹å‡»â€œç¡®å®šâ€å¼€å§‹ä¸‹è½½ã€‚\n"\
                         L"è¯·åœ¨å®‰è£… Java å®ŒæˆåŽé‡æ–°å¯åŠ¨ HMCLã€‚\n"\
                         L"ç‚¹å‡»â€œå¸®åŠ©â€å¯»æ±‚å¸®åŠ©ã€‚"
+=======
+#define ERROR_PROMPT_ZH L"ÔËÐÐ HMCL ÒÔ¼° Minecraft ÐèÒª Java ÔËÐÐÊ±»·¾³£¬µã»÷¡°È·¶¨¡±¿ªÊ¼ÏÂÔØ¡£\n"\
+                        L"ÇëÔÚ°²×° Java Íê³ÉºóÖØÐÂÆô¶¯ HMCL¡£\n"\
+                        L"µã»÷¡°°ïÖú¡±Ñ°Çó°ïÖú¡£"
+>>>>>>> Stashed changes
