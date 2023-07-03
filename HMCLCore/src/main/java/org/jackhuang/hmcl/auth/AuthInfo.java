57519efc811d2590116a85377802980516464c88
sheip9 <sheip9@outlook.com>
2023-07-03 14:38:03 +0800

    fix yggdrasil login

--- HMCLCore/src/main/java/org/jackhuang/hmcl/auth/AuthInfo.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/auth/AuthInfo.java
@@ -31,7 +31,7 @@ import java.util.UUID;
 @Immutable
 public class AuthInfo implements AutoCloseable {
     public static final String USER_TYPE_MSA = "msa";
-    public static final String USER_TYPE_MOJANG = "mojang";
+    public static final String USER_TYPE_MOJANG = "Mojang";
     public static final String USER_TYPE_LEGACY = "legacy";
 
 
