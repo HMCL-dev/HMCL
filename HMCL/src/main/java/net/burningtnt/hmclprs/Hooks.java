a84da6c1926ab5f6e8670070e60ddcd935546c2d
burningtnt <pangyl08@163.com>
2023-12-23 19:47:15 +0800

    Fix

--- HMCL/src/main/java/net/burningtnt/hmclprs/Hooks.java
+++ HMCL/src/main/java/net/burningtnt/hmclprs/Hooks.java
@@ -1,10 +1,6 @@
 package net.burningtnt.hmclprs;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
 import javax.swing.*;
-import java.util.Optional;
 
 import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
 
@@ -13,17 +9,10 @@ public final class Hooks {
     }
 
     public static void onApplicationLaunch() {
-        try {
-            Optional<ButtonType> opt = new Alert(Alert.AlertType.CONFIRMATION, i18n("prs.warning")).showAndWait();
-            if (opt.isPresent() && opt.get() == ButtonType.YES) {
-                return;
-            }
-        } catch (Throwable ignored) {
-            if (JOptionPane.showConfirmDialog(
-                    null, i18n("prs.warning"), i18n("message.warning"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE
-            ) == JOptionPane.OK_OPTION) {
-                return;
-            }
+        if (JOptionPane.showConfirmDialog(
+                null, i18n("prs.warning"), i18n("message.warning"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE
+        ) == JOptionPane.OK_OPTION) {
+            return;
         }
         System.exit(1);
     }
