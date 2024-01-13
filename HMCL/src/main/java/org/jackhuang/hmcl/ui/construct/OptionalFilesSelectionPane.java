addce8b9fe0e47de55566c88ba6e04297334e755
CaveNightingale <cavenightingale@foxmail.com>
2024-01-13 14:48:25 +0800

    copyright

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/OptionalFilesSelectionPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/OptionalFilesSelectionPane.java
@@ -1,14 +1,28 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2024  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.ui.construct;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXListView;
-import com.sun.javafx.binding.Logging;
 import javafx.geometry.Pos;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import org.jackhuang.hmcl.mod.ModpackFile;
 import org.jackhuang.hmcl.mod.RemoteMod;
--- HMCLCore/src/main/java/org/jackhuang/hmcl/mod/ModpackFile.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/mod/ModpackFile.java
@@ -1,3 +1,20 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2024  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.mod;
 
 import org.jetbrains.annotations.Nullable;
@@ -22,10 +39,13 @@ public interface ModpackFile {
     String getPath();
     /**
      * Return the mod the file belongs to
-     *
+     * <p>
      * About null and Optional.empty():
-     * If the file hasn't been queried from remote, the mod will be null
-     * If the file has been queried from remote but not found, the mod will be Optional.empty()
+     * <ul>
+     * <li> If the file hasn't been queried from remote, the mod will be null </li>
+     * <li> If the file has been queried from remote but not found, the mod will be Optional.empty() </li>
+     * </ul>
+     * </p>
      */
     @Nullable Optional<RemoteMod> getMod();
 }
