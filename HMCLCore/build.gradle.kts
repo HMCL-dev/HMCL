96270b170642859386ce79e7f36e9de7971685ae
Glavo <zjx001202@gmail.com>
2024-01-19 23:23:44 +0800

    Bump dependencies (#2659)
    * Bump dependencies
    
    * Fix build

--- HMCLCore/build.gradle.kts
+++ HMCLCore/build.gradle.kts
@@ -1,5 +1,3 @@
-import kotlin.streams.toList
-
 plugins {
     `java-library`
 }
@@ -13,8 +11,8 @@ dependencies {
     api("org.jenkins-ci:constant-pool-scanner:1.2")
     api("com.github.steveice10:opennbt:1.5")
     api("org.nanohttpd:nanohttpd:2.3.1")
-    api("org.apache.commons:commons-compress:1.23.0")
-    compileOnlyApi("org.jetbrains:annotations:24.0.1")
+    api("org.apache.commons:commons-compress:1.25.0")
+    compileOnlyApi("org.jetbrains:annotations:24.1.0")
     compileOnlyApi("com.github.burningtnt:BytecodeImplGenerator:b45b6638eeaeb903aa22ea947d37c45e5716a18c")
 }
 
@@ -28,7 +26,7 @@ tasks.getByName<JavaCompile>("compileJava") {
             classpath(project.sourceSets["main"].compileClasspath)
             mainClass.set("net.burningtnt.bcigenerator.BytecodeImplGenerator")
             System.getProperty("bci.debug.address")?.let { address -> jvmArgs("-agentlib:jdwp=transport=dt_socket,server=n,address=$address,suspend=y") }
-            args(bytecodeClasses.stream().map { s -> project.layout.buildDirectory.file("classes/java/main/$s.class").get().asFile.path }.toList())
+            args(bytecodeClasses.map { s -> project.layout.buildDirectory.file("classes/java/main/$s.class").get().asFile.path })
         }
     }
 }
