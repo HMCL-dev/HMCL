2e3800a0894e1e426d04d11dec80c15abbca19ba
Glavo <zjx001202@gmail.com>
2024-01-29 02:51:37 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/versioning/GameVersionNumber.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/versioning/GameVersionNumber.java
@@ -1,12 +1,10 @@
 package org.jackhuang.hmcl.util.versioning;
 
-import org.jackhuang.hmcl.util.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
