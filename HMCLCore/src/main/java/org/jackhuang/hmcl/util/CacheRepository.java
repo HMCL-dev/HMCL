5bdb2547df3a17f167776b4b63f8d522b1421de2
zkitefly <z18344203426@qq.com>
2024-02-07 21:33:16 +0800

    Update CacheRepository.java

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/CacheRepository.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/CacheRepository.java
@@ -288,7 +288,7 @@ public class CacheRepository {
 
     public void saveETagIndex() throws IOException {
         if (!indexFile.toFile().exists())
-            FileUtils.writeText(indexFile, "");
+            indexFile.toFile().createNewFile();
         try (FileChannel channel = FileChannel.open(indexFile, StandardOpenOption.READ, StandardOpenOption.WRITE)) {
             FileLock lock = channel.lock();
             try {
