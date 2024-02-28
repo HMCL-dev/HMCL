ea0f720afb6eb429704741baaa1a5056b65d35a8
Glavo <zjx001202@gmail.com>
2024-02-28 20:17:37 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/upgrade/UpdateChecker.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/upgrade/UpdateChecker.java
@@ -43,9 +43,10 @@ public final class UpdateChecker {
                 RemoteVersion latest = latestVersion.get();
                 if (latest == null || isDevelopmentVersion(Metadata.VERSION)) {
                     return false;
-                } else if (latest.isForce() || latest.getChannel() != UpdateChannel.getChannel()) {
-                    // We can update from development version to stable version,
-                    // which can be downgrading.
+                } else if (latest.isForce()
+                        || Metadata.isNightly()
+                        || latest.getChannel() == UpdateChannel.NIGHTLY
+                        || latest.getChannel() != UpdateChannel.getChannel()) {
                     return !latest.getVersion().equals(Metadata.VERSION);
                 } else {
                     return VersionNumber.compare(Metadata.VERSION, latest.getVersion()) < 0;
