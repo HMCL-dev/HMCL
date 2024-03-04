b6429ed56eb7f32aa80a495b7581e7835b8021e0
ShulkerSakura <2531493755@qq.com>
2024-03-04 14:04:04 +0800

    Style Checked

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/io/VirtualizedSystemDetection.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/io/VirtualizedSystemDetection.java
@@ -4,25 +4,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class VirtualizedSystemDetection {
-    public static boolean isChromeOSContainer(){
+public final class VirtualizedSystemDetection {
+    public static boolean isChromeOSContainer() {
         String isVirtualSystemProductPath = "/sys/devices/virtual/dmi/id/";
         String isChromeOSContainerProductFile = "product_name";
-        if(Files.exists(Paths.get(isVirtualSystemProductPath))){
-            try{
-                String isChromeOS = new String(Files.readAllBytes(Paths.get(isVirtualSystemProductPath+isChromeOSContainerProductFile)));
-                if (isChromeOS.contains("crosvm")){
+        if (Files.exists(Paths.get(isVirtualSystemProductPath))) {
+            try {
+                String isChromeOS = new String(Files.readAllBytes(Paths.get(isVirtualSystemProductPath + isChromeOSContainerProductFile)));
+                if (isChromeOS.contains("crosvm")) {
                     //LOG.log(Level.INFO,"Running Under Chrome OS Linux Container");
                     System.out.println("Running Under Chrome OS Linux Container");
                     return true;
-                }else {
+                } else {
                     return false;
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
                 return false;
             }
-        }else {
+        } else {
             return false;
         }
     }
