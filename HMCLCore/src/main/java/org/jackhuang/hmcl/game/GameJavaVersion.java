2d56da0ca27cb77704b0aa5054b6c3f71dcd81f9
Glavo <zjx001202@gmail.com>
2024-03-05 00:32:11 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/game/GameJavaVersion.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/game/GameJavaVersion.java
@@ -17,16 +17,15 @@
  */
 package org.jackhuang.hmcl.game;
 
-public enum GameJavaVersion {
-    JAVA_8("jre-legacy", 8),
-    JAVA_16("java-runtime-alpha", 16),
-    JAVA_17("java-runtime-beta", 17),
-    JAVA_21("java-runtime-delta", 21)
-    ;
+public class GameJavaVersion {
     private final String component;
     private final int majorVersion;
 
-    private GameJavaVersion(String component, int majorVersion) {
+    public GameJavaVersion() {
+        this("", 0);
+    }
+
+    public GameJavaVersion(String component, int majorVersion) {
         this.component = component;
         this.majorVersion = majorVersion;
     }
@@ -38,4 +37,9 @@ public enum GameJavaVersion {
     public int getMajorVersion() {
         return majorVersion;
     }
+
+    public static final GameJavaVersion JAVA_21 = new GameJavaVersion("java-runtime-delta", 21);
+    public static final GameJavaVersion JAVA_17 = new GameJavaVersion("java-runtime-beta", 17);
+    public static final GameJavaVersion JAVA_16 = new GameJavaVersion("java-runtime-alpha", 16);
+    public static final GameJavaVersion JAVA_8 = new GameJavaVersion("jre-legacy", 8);
 }
