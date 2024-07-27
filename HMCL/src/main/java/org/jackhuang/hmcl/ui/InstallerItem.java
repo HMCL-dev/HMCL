3ebbdb60ef730f89fd8bef7e9c810aaf12de8e05
Glavo <zjx001202@gmail.com>
2024-07-28 02:47:52 +0800

    Fix #3191: 修复无法同时安装 LiteLoader 与 Forge 的问题 (#3198)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/InstallerItem.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/InstallerItem.java
@@ -224,7 +224,8 @@ public class InstallerItem extends Control {
             InstallerItem quiltApi = new InstallerItem(QUILT_API, style);
 
             Map<InstallerItem, Set<InstallerItem>> incompatibleMap = new HashMap<>();
-            mutualIncompatible(incompatibleMap, forge, fabric, quilt, neoForge, liteLoader);
+            mutualIncompatible(incompatibleMap, forge, fabric, quilt, neoForge);
+            addIncompatibles(incompatibleMap, liteLoader, fabric, quilt, neoForge);
             addIncompatibles(incompatibleMap, optiFine, fabric, quilt, neoForge);
             addIncompatibles(incompatibleMap, fabricApi, forge, quiltApi, neoForge, liteLoader, optiFine);
             addIncompatibles(incompatibleMap, quiltApi, forge, fabric, fabricApi, neoForge, liteLoader, optiFine);
