3ea90be02b846767c78832d7b054bf28af63e845
Glavo <zjx001202@gmail.com>
2024-07-29 15:44:44 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/InstallerItem.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/InstallerItem.java
@@ -299,7 +299,7 @@ public class InstallerItem extends Control {
             Pane pane;
             if (control.style == Style.CARD) {
                 pane = new VBox();
-                holder.add(FXUtils.onWeakChangeAndOperate(pane.widthProperty(), v -> FXUtils.setLimitHeight(pane, v.doubleValue() * 0.7)));
+                holder.add(FXUtils.onWeakChange(pane.widthProperty(), v -> FXUtils.setLimitHeight(pane, v.doubleValue() * 0.7)));
             } else {
                 pane = new HBox();
             }
