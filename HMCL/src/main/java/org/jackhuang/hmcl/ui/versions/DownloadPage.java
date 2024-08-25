df31e1f46f6b781c9868f12fca4bfb36c0ed3c59
ShulkerSakura <2531493755@qq.com>
2024-08-25 08:15:20 +0800

    更改了i18n key并添加了新的词条

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadPage.java
@@ -457,7 +457,7 @@ public class DownloadPage extends Control implements DecoratorPage {
 
             this.setBody(box);
 
-            JFXButton downloadButton = new JFXButton(i18n("download"));
+            JFXButton downloadButton = new JFXButton(i18n("mods.install"));
             downloadButton.getStyleClass().add("dialog-accept");
             downloadButton.setOnAction(e -> {
                 if (!spinnerPane.isLoading() && spinnerPane.getFailedReason() == null) {
@@ -466,7 +466,7 @@ public class DownloadPage extends Control implements DecoratorPage {
                 selfPage.download(version);
             });
 
-            JFXButton saveAsButton = new JFXButton(i18n("button.save_as"));
+            JFXButton saveAsButton = new JFXButton(i18n("mods.save_as"));
             saveAsButton.getStyleClass().add("dialog-accept");
             saveAsButton.setOnAction(e -> {
                 if (!spinnerPane.isLoading() && spinnerPane.getFailedReason() == null) {
