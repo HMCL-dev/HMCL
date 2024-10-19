25c669fd62ce67479cdfbd365fc2ffc10452abf9
Glavo <zjx001202@gmail.com>
2024-10-19 21:59:49 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/WorldInfoPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/WorldInfoPage.java
@@ -1,7 +1,25 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2024 huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.ui.versions;
 
 import com.github.steveice10.opennbt.tag.builtin.*;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXSpinner;
 import com.jfoenix.controls.JFXTextField;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
@@ -15,6 +33,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import org.jackhuang.hmcl.game.World;
+import org.jackhuang.hmcl.task.Schedulers;
+import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.ui.FXUtils;
 import org.jackhuang.hmcl.ui.construct.ComponentList;
 import org.jackhuang.hmcl.ui.construct.DoubleValidator;
@@ -32,18 +52,33 @@ import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 import static org.jackhuang.hmcl.util.i18n.I18n.formatDateTime;
 import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
 
+/**
+ * @author Glavo
+ */
 public final class WorldInfoPage extends StackPane implements DecoratorPage {
     private final World world;
-    private final CompoundTag levelDat;
-    private final CompoundTag dataTag;
+    private CompoundTag levelDat;
+    private CompoundTag dataTag;
 
     private final ObjectProperty<State> stateProperty = new SimpleObjectProperty<>();
 
-    public WorldInfoPage(World world) throws IOException {
+    public WorldInfoPage(World world) {
         this.world = world;
-        this.levelDat = world.readLevelDat();
-        this.dataTag = levelDat.get("Data");
 
+        this.getChildren().add(new JFXSpinner());
+        Task.supplyAsync(world::readLevelDat)
+                .whenComplete(Schedulers.javafx(), ((result, exception) -> {
+                    if (exception == null) {
+                        this.levelDat = result;
+                        this.dataTag = levelDat.get("Data");
+                        loadWorldInfo();
+                    } else {
+                        LOG.warning("Failed to load level dat", exception);
+                    }
+                })).start();
+    }
+
+    private void loadWorldInfo() {
         CompoundTag worldGenSettings = dataTag.get("WorldGenSettings");
 
         stateProperty.set(State.fromTitle(i18n("world.info.title", world.getWorldName())));
