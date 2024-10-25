e051c212d1a09b37694bcd56f973f8d98fa482d3
Glavo <zjx001202@gmail.com>
2024-10-25 12:36:07 +0800

    支持使用鼠标返回键返回上级 (#3381)
    * 支持使用鼠标返回键返回上级
    
    * fix checkstyle

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
@@ -30,6 +30,8 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -144,6 +146,18 @@ public class DecoratorController {
 
         // press ESC to go back
         onEscPressed(navigator, this::back);
+
+        try {
+            // For JavaFX 12+
+            MouseButton button = MouseButton.valueOf("BACK");
+            navigator.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+                if (e.getButton() == button) {
+                    back();
+                    e.consume();
+                }
+            });
+        } catch (IllegalArgumentException ignored) {
+        }
     }
 
     public Decorator getDecorator() {
