ba617d0cf56a3cd0b7163aa887a36c3e342e4cb9
Glavo <zjx001202@gmail.com>
2024-10-26 20:51:40 +0800

    Bump XZ to 1.10

--- HMCLCore/build.gradle.kts
+++ HMCLCore/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     api("org.glavo:simple-png-javafx:0.3.0")
     api("com.google.code.gson:gson:2.11.0")
     api("com.moandjiezana.toml:toml4j:0.7.2")
-    api("org.tukaani:xz:1.9")
+    api("org.tukaani:xz:1.10")
     api("org.hildan.fxgson:fx-gson:5.0.0")
     api("org.jenkins-ci:constant-pool-scanner:1.2")
     api("com.github.steveice10:opennbt:1.5")
