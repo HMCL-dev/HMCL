0572845a840573dc9347389f730d15272d7edf52
Glavo <zjx001202@gmail.com>
2024-11-08 14:13:55 +0800

    将 VERSION_ROOT 更新至 3.6

--- HMCL/build.gradle.kts
+++ HMCL/build.gradle.kts
@@ -25,7 +25,7 @@ val buildNumber = System.getenv("BUILD_NUMBER")?.toInt().let { number ->
         if (!shortCommit.isNullOrEmpty()) "$prefix-$shortCommit" else "SNAPSHOT"
     }
 }
-val versionRoot = System.getenv("VERSION_ROOT") ?: "3.5"
+val versionRoot = System.getenv("VERSION_ROOT") ?: "3.6"
 val versionType = System.getenv("VERSION_TYPE") ?: if (isOfficial) "nightly" else "unofficial"
 
 val microsoftAuthId = System.getenv("MICROSOFT_AUTH_ID") ?: ""
