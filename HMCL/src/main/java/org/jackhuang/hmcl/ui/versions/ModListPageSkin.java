fdb222c614d6e619bf7be704ae7fd868bb5bdc21
Zkitefly <64117916+zkitefly@users.noreply.github.com>
2025-02-14 16:03:10 +0000

    fix

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
@@ -80,7 +80,6 @@ import org.jackhuang.hmcl.util.logging.Level;
 import org.jetbrains.annotations.NotNull;
 import javafx.scene.Node;
 import javafx.scene.control.SkinBase;
-import javafx.scene.layout.Region;
 
 import static org.jackhuang.hmcl.ui.FXUtils.ignoreEvent;
 import static org.jackhuang.hmcl.ui.FXUtils.onEscPressed;
