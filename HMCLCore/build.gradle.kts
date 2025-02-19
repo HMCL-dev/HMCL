d40742a6240e3677ab0d0a6a907ff3a9651fce87
Glavo <zjx001202@gmail.com>
2025-02-19 16:28:46 +0800

    Bump jsoup to 1.18.3 (#3632)

--- HMCLCore/build.gradle.kts
+++ HMCLCore/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     api("com.github.steveice10:opennbt:1.5")
     api("org.nanohttpd:nanohttpd:2.3.1")
     api("org.apache.commons:commons-compress:1.25.0")
-    api("org.jsoup:jsoup:1.18.1")
+    api("org.jsoup:jsoup:1.18.3")
     compileOnlyApi("org.jetbrains:annotations:26.0.1")
 
     if (JavaVersion.current().isJava8) {
