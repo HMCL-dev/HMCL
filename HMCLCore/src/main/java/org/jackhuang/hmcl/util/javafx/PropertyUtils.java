76a58f5500399752e4ccf2f52b385d75d9386392
Glavo <zjx001202@gmail.com>
2025-02-19 09:40:29 +0800

    fix checkstyle

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/javafx/PropertyUtils.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/javafx/PropertyUtils.java
@@ -22,7 +22,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
