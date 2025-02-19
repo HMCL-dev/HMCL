52fa3ebce6ddc5dafa72da739b5dfb4bc5b5ad7b
Glavo <zjx001202@gmail.com>
2025-02-19 11:03:46 +0800

    删除全局游戏设置页面的离线账户更换皮肤提示

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/VersionSettingsPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/VersionSettingsPage.java
@@ -135,10 +135,6 @@ public final class VersionSettingsPage extends StackPane implements DecoratorPag
         rootPane.getStyleClass().add("card-list");
 
         if (globalSetting) {
-            HintPane skinHint = new HintPane(MessageDialogPane.MessageType.INFO);
-            skinHint.setText(i18n("settings.skin"));
-            rootPane.getChildren().add(skinHint);
-
             HintPane specificSettingsHint = new HintPane(MessageDialogPane.MessageType.WARNING);
             Text text = new Text();
             text.textProperty().bind(BindingMapping.of(selectedVersion)
