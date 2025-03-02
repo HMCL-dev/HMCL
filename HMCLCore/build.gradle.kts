4292d47fe86840d38dfbe7a77c7863c3b1e13bcc
Glavo <zjx001202@gmail.com>
2025-03-02 23:58:07 +0800

    Bump Gson to 2.12.1 (#3691)

--- HMCLCore/build.gradle.kts
+++ HMCLCore/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     api("org.glavo:simple-png-javafx:0.3.0")
-    api("com.google.code.gson:gson:2.11.0")
+    api("com.google.code.gson:gson:2.12.1")
     api("com.moandjiezana.toml:toml4j:0.7.2")
     api("org.tukaani:xz:1.10")
     api("org.hildan.fxgson:fx-gson:5.0.0")
