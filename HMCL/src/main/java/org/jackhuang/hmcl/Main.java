1d72045ef4e223132f020938bf335c3e63a8260f
Glavo <zjx001202@gmail.com>
2025-04-30 11:35:12 +0800

    update i18n

--- HMCL/src/main/java/org/jackhuang/hmcl/Main.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Main.java
@@ -58,21 +58,7 @@ public final class Main {
         System.getProperties().putIfAbsent("javafx.autoproxy.disable", "true");
         System.getProperties().putIfAbsent("http.agent", "HMCL/" + Metadata.VERSION);
 
-        if (!Files.isDirectory(Metadata.HMCL_CURRENT_DIRECTORY)) {
-            try {
-                Files.createDirectories(Metadata.HMCL_CURRENT_DIRECTORY);
-                if (OperatingSystem.CURRENT_OS == OperatingSystem.WINDOWS) {
-                    try {
-                        Files.setAttribute(Metadata.HMCL_CURRENT_DIRECTORY, "dos:hidden", true);
-                    } catch (IOException e) {
-                        LOG.warning("Failed to set hidden attribute of " + Metadata.HMCL_CURRENT_DIRECTORY, e);
-                    }
-                }
-            } catch (IOException e) {
-                showErrorAndExit("Failed to crate HMCL directory."); // TODO: i18n
-            }
-        }
-
+        createHMCLCurrentDirectory();
         LOG.start(Metadata.HMCL_CURRENT_DIRECTORY.resolve("logs"));
 
         checkDirectoryPath();
@@ -95,6 +81,23 @@ public final class Main {
     public static void exit(int exitCode) {
         LOG.shutdown();
         System.exit(exitCode);
+    }
+
+    private static void createHMCLCurrentDirectory() {
+        if (!Files.isDirectory(Metadata.HMCL_CURRENT_DIRECTORY)) {
+            try {
+                Files.createDirectories(Metadata.HMCL_CURRENT_DIRECTORY);
+                if (OperatingSystem.CURRENT_OS == OperatingSystem.WINDOWS) {
+                    try {
+                        Files.setAttribute(Metadata.HMCL_CURRENT_DIRECTORY, "dos:hidden", true);
+                    } catch (IOException e) {
+                        LOG.warning("Failed to set hidden attribute of " + Metadata.HMCL_CURRENT_DIRECTORY, e);
+                    }
+                }
+            } catch (IOException e) {
+                showErrorAndExit(i18n("fatal.create_hmcl_current_directory_failure", Metadata.HMCL_CURRENT_DIRECTORY));
+            }
+        }
     }
 
     private static void initIcon() {
