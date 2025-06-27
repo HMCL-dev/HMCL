21adeccb1aab65126d20e249fde3c85a741f4990
Glavo <zjx001202@gmail.com>
2025-06-27 19:10:29 +0800

    update

--- buildSrc/src/main/java/org/jackhuang/hmcl/gradle/mod/ParseModDataTask.java
+++ buildSrc/src/main/java/org/jackhuang/hmcl/gradle/mod/ParseModDataTask.java
@@ -1,3 +1,20 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2025 huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.gradle.mod;
 
 import com.google.gson.*;
@@ -24,6 +41,9 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author Glavo
+ */
 public abstract class ParseModDataTask extends DefaultTask {
 
     @InputFile
@@ -74,10 +94,10 @@ public abstract class ParseModDataTask extends DefaultTask {
         return "";
     }
 
-    private static final Set<String> skip = new HashSet<>(Arrays.asList(
+    private static final Set<String> skip = Set.of(
             "Minecraft",
             "The Building Game"
-    ));
+    );
 
     @TaskAction
     public void run() throws IOException {
