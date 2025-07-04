9a83e60674e409eaf2a15a7f900d9e1eb3ec914f
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-07-05 00:10:36 +0800

    checkStyle

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/account/MicrosoftAccountSkinPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/account/MicrosoftAccountSkinPane.java
@@ -25,7 +25,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import org.jackhuang.hmcl.auth.microsoft.MicrosoftAccount;
