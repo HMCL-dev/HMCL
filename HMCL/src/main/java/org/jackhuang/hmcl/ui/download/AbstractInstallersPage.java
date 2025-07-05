c669026312e102eaf880d162ed95d6fccf16bbf2
Glavo <zjx001202@gmail.com>
2025-07-05 22:29:07 +0800

    Fix #4067: 修复使用 JavaFX 25 启动时下载界面布局错位的问题 (#4069)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/download/AbstractInstallersPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/download/AbstractInstallersPage.java
@@ -121,19 +121,14 @@ public abstract class AbstractInstallersPage extends Control implements WizardPa
                 InstallerItem[] libraries = control.group.getLibraries();
 
                 FlowPane libraryPane = new FlowPane(libraries);
-                libraryPane.setVgap(16);
+                libraryPane.setVgap(8);
                 libraryPane.setHgap(16);
 
-                if (libraries.length <= 8) {
-                    BorderPane.setMargin(libraryPane, new Insets(16, 0, 16, 0));
-                    root.setCenter(libraryPane);
-                } else {
-                    ScrollPane scrollPane = new ScrollPane(libraryPane);
-                    scrollPane.setFitToWidth(true);
-                    scrollPane.setFitToHeight(true);
-                    BorderPane.setMargin(scrollPane, new Insets(16, 0, 16, 0));
-                    root.setCenter(scrollPane);
-                }
+                ScrollPane scrollPane = new ScrollPane(libraryPane);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                BorderPane.setMargin(scrollPane, new Insets(16, 0, 16, 0));
+                root.setCenter(scrollPane);
             }
 
             {
