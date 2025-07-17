447583d07c584ec981eb88be0789c8bd4aa46709
ShulkerSakura <2531493755@qq.com>
2025-07-17 14:31:17 +0800

    Delete test code

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/account/CreateAccountPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/account/CreateAccountPane.java
@@ -268,9 +268,6 @@ public class CreateAccountPane extends JFXDialogLayout implements DialogAware {
         };
 
         if (factory instanceof OfflineAccountFactory && username != null && !USERNAME_CHECKER_PATTERN.matcher(username).matches() || username.length() > 16) {
-            System.out.println(username);
-            System.out.println("USERNAME LENGTH: " + username.length());
-            System.out.println(username.length() > 16);
             JFXButton btnYes = new JFXButton(i18n("button.ok"));
             btnYes.getStyleClass().add("dialog-error");
             btnYes.setOnAction(e -> doCreate.run());
