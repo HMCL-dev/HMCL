43b769923b3c7753d1c361403072c5fd1b66008d
Glavo <zjx001202@gmail.com>
2025-07-26 19:49:05 +0800

    使用 readAllBytes 和 transferTo 简化代码 (#4115)

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/io/IOUtils.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/io/IOUtils.java
@@ -62,54 +62,18 @@ public final class IOUtils {
         }
     }
 
-    /**
-     * Read all bytes to a buffer from given input stream. The stream will not be closed.
-     *
-     * @param stream the InputStream being read.
-     * @return all bytes read from the stream
-     * @throws IOException if an I/O error occurs.
-     */
-    public static byte[] readFullyWithoutClosing(InputStream stream) throws IOException {
-        ByteArrayOutputStream result = new ByteArrayOutputStream(Math.max(stream.available(), 32));
-        copyTo(stream, result);
-        return result.toByteArray();
-    }
-
-    public static String readFullyAsStringWithClosing(InputStream stream) throws IOException {
-        ByteArrayOutputStream result = new ByteArrayOutputStream(Math.max(stream.available(), 32));
-        copyTo(stream, result);
-        return result.toString(UTF_8);
-    }
-
-    /**
-     * Read all bytes to a buffer from given input stream, and close the input stream finally.
-     *
-     * @param stream the InputStream being read, closed finally.
-     * @return all bytes read from the stream
-     * @throws IOException if an I/O error occurs.
-     */
-    public static ByteArrayOutputStream readFully(InputStream stream) throws IOException {
-        try (InputStream is = stream) {
-            ByteArrayOutputStream result = new ByteArrayOutputStream(Math.max(is.available(), 32));
-            copyTo(is, result);
-            return result;
+    public static byte[] readFully(InputStream stream) throws IOException {
+        try (stream) {
+            return stream.readAllBytes();
         }
     }
 
-    public static byte[] readFullyAsByteArray(InputStream stream) throws IOException {
-        return readFully(stream).toByteArray();
-    }
-
     public static String readFullyAsString(InputStream stream) throws IOException {
-        return readFully(stream).toString(UTF_8);
+        return new String(readFully(stream), UTF_8);
     }
 
     public static String readFullyAsString(InputStream stream, Charset charset) throws IOException {
-        return readFully(stream).toString(charset);
-    }
-
-    public static void copyTo(InputStream src, OutputStream dest) throws IOException {
-        copyTo(src, dest, new byte[DEFAULT_BUFFER_SIZE]);
+        return new String(readFully(stream), charset);
     }
 
     public static void copyTo(InputStream src, OutputStream dest, byte[] buf) throws IOException {
