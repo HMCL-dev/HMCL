d9ab5769e877898246853a137498ae8eec351653
Glavo <zjx001202@gmail.com>
2025-08-06 15:38:10 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
@@ -63,7 +63,6 @@ import org.jackhuang.hmcl.util.io.FileUtils;
 import org.jackhuang.hmcl.util.io.NetworkUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
