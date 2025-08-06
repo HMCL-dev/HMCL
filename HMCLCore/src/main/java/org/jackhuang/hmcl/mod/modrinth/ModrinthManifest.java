45a9627b569c8de1044c2c9cdd98250e2f78f883
Glavo <zjx001202@gmail.com>
2025-08-06 20:21:49 +0800

    修复 #3716 与 main 的冲突

--- HMCLCore/src/main/java/org/jackhuang/hmcl/mod/modrinth/ModrinthManifest.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/mod/modrinth/ModrinthManifest.java
@@ -24,7 +24,6 @@ import org.jackhuang.hmcl.util.gson.TolerableValidationException;
 import org.jackhuang.hmcl.util.gson.Validation;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -98,10 +97,10 @@ public class ModrinthManifest implements ModpackManifest, Validation {
         private final String path;
         private final Map<String, String> hashes;
         private final Map<String, String> env;
-        private final List<URI> downloads;
+        private final List<String> downloads;
         private final int fileSize;
 
-        public File(String path, Map<String, String> hashes, Map<String, String> env, List<URI> downloads, int fileSize) {
+        public File(String path, Map<String, String> hashes, Map<String, String> env, List<String> downloads, int fileSize) {
             this.path = path;
             this.hashes = hashes;
             this.env = env;
@@ -121,7 +120,7 @@ public class ModrinthManifest implements ModpackManifest, Validation {
             return env;
         }
 
-        public List<URI> getDownloads() {
+        public List<String> getDownloads() {
             return downloads;
         }
 
