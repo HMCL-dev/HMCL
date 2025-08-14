f6a3b76a0b558fcbf9a88d45b42b193eb40ce081
Glavo <zjx001202@gmail.com>
2025-08-14 20:20:46 +0800

    暂时禁用 MCIM 加速 (#4265)

--- HMCLCore/src/main/java/org/jackhuang/hmcl/download/BMCLAPIDownloadProvider.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/BMCLAPIDownloadProvider.java
@@ -78,14 +78,13 @@ public final class BMCLAPIDownloadProvider implements DownloadProvider {
                 pair("https://maven.fabricmc.net", apiRoot + "/maven"),
                 pair("https://authlib-injector.yushi.moe", apiRoot + "/mirrors/authlib-injector"),
                 pair("https://repo1.maven.org/maven2", "https://mirrors.cloud.tencent.com/nexus/repository/maven-public"),
-                pair("https://zkitefly.github.io/unlisted-versions-of-minecraft", "https://alist.8mi.tech/d/mirror/unlisted-versions-of-minecraft/Auto"),
-
-                // https://github.com/mcmod-info-mirror/mcim-rust-api
-                pair("https://api.modrinth.com", "https://mod.mcimirror.top/modrinth"),
-                pair("https://cdn.modrinth.com", "https://mod.mcimirror.top"),
-                pair("https://api.curseforge.com", "https://mod.mcimirror.top/curseforge"),
-                pair("https://edge.forgecdn.net", "https://mod.mcimirror.top"),
-                pair("https://mediafilez.forgecdn.net", "https://mod.mcimirror.top")
+                pair("https://zkitefly.github.io/unlisted-versions-of-minecraft", "https://alist.8mi.tech/d/mirror/unlisted-versions-of-minecraft/Auto")
+//                // https://github.com/mcmod-info-mirror/mcim-rust-api
+//                pair("https://api.modrinth.com", "https://mod.mcimirror.top/modrinth"),
+//                pair("https://cdn.modrinth.com", "https://mod.mcimirror.top"),
+//                pair("https://api.curseforge.com", "https://mod.mcimirror.top/curseforge"),
+//                pair("https://edge.forgecdn.net", "https://mod.mcimirror.top"),
+//                pair("https://mediafilez.forgecdn.net", "https://mod.mcimirror.top")
         );
     }
 
