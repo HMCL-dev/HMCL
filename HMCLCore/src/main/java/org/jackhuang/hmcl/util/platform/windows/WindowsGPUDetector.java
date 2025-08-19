74d0ae1c91a7995820ad45a6f2d74667bfd65e36
Glavo <zjx001202@gmail.com>
2025-08-19 21:15:07 +0800

    Update WindowsGPUDetector.java

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/platform/windows/WindowsGPUDetector.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/platform/windows/WindowsGPUDetector.java
@@ -27,7 +27,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -95,14 +95,8 @@ final class WindowsGPUDetector {
         } else if (object instanceof String[]) {
             return String.join(" ", (String[]) object);
         } else if (object instanceof byte[]) {
-            String value = null;
-            try {
-                value = new String((byte[])object, "UTF-16LE");
-                value = value.replaceAll("\0", "");
-                return value;
-            } catch (UnsupportedEncodingException e) {
-                return object.toString();
-            }
+            return new String((byte[]) object, StandardCharsets.UTF_16LE)
+                    .replace("\0", "");
         } else {
             return object.toString();
         }
