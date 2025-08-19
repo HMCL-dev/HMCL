47e93bfe96605a34c51edfd7ad9af708d38b85eb
Henryxjh <xu2004082604@outlook.com>
2025-08-19 17:23:40 +0800

    在修复代码中指定字符集并去除"\0"

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/platform/windows/WindowsGPUDetector.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/platform/windows/WindowsGPUDetector.java
@@ -27,6 +27,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -94,7 +95,14 @@ final class WindowsGPUDetector {
         } else if (object instanceof String[]) {
             return String.join(" ", (String[]) object);
         } else if (object instanceof byte[]) {
-            return new String((byte[])object);
+            String value = null;
+            try {
+                value = new String((byte[])object, "UTF-16LE");
+                value = value.replaceAll("\0", "");
+                return value;
+            } catch (UnsupportedEncodingException e) {
+                return object.toString();
+            }
         } else {
             return object.toString();
         }
