af560961626e6e7daa0470933e58a224d8dc4c51
Glavo <zjx001202@gmail.com>
2025-08-20 20:33:03 +0800

    优化对于 IntelliJ IDEA 的支持 (#4270)

--- HMCL/src/main/java/org/jackhuang/hmcl/util/SelfDependencyPatcher.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/util/SelfDependencyPatcher.java
@@ -46,7 +46,6 @@ import org.jackhuang.hmcl.Metadata;
 import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.io.ChecksumMismatchException;
 import org.jackhuang.hmcl.util.io.IOUtils;
-import org.jackhuang.hmcl.util.io.JarUtils;
 import org.jackhuang.hmcl.java.JavaRuntime;
 import org.jackhuang.hmcl.util.platform.Platform;
 
@@ -63,6 +62,7 @@ import java.util.List;
 import java.util.*;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.Manifest;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.stream.Collectors.toSet;
@@ -261,9 +261,15 @@ public final class SelfDependencyPatcher {
                 .map(DependencyDescriptor::localPath)
                 .toArray(Path[]::new);
 
-        String[] addOpens = JarUtils.getManifestAttribute("Add-Opens", "").split(" ");
+        String addOpens = null;
+        try (InputStream input = SelfDependencyPatcher.class.getResourceAsStream("/META-INF/MANIFEST.MF")) {
+            if (input != null)
+                addOpens = new Manifest(input).getMainAttributes().getValue("Add-Opens");
+        } catch (IOException e) {
+            LOG.warning("Failed to read MANIFEST.MF file", e);
+        }
 
-        JavaFXPatcher.patch(modules, jars, addOpens);
+        JavaFXPatcher.patch(modules, jars, addOpens != null ? addOpens.split(" ") : new String[0]);
     }
 
     /**
