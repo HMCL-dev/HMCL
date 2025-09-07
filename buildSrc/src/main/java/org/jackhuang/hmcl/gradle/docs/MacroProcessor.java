583cf5f8bf401b8d3182663b9dc1f91cd625e455
Glavo <zjx001202@gmail.com>
2025-09-07 17:19:20 +0800

    update

--- buildSrc/src/main/java/org/jackhuang/hmcl/gradle/docs/MacroProcessor.java
+++ buildSrc/src/main/java/org/jackhuang/hmcl/gradle/docs/MacroProcessor.java
@@ -17,14 +17,23 @@
  */
 package org.jackhuang.hmcl.gradle.docs;
 
-import javax.print.Doc;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /// @author Glavo
 public enum MacroProcessor {
+    BLOCK {
+        @Override
+        public void apply(Document document, Document.MacroBlock macroBlock, StringBuilder outputBuilder) throws IOException {
+            MacroProcessor.writeBegin(outputBuilder, macroBlock);
+            MacroProcessor.writeProperties(outputBuilder, macroBlock);
+            for (String line : macroBlock.contentLines()) {
+                outputBuilder.append(line).append('\n');
+            }
+            MacroProcessor.writeEnd(outputBuilder, macroBlock);
+        }
+    },
     LANGUAGE_SWITCHER {
         private static <T> boolean containsIdentity(List<T> list, T element) {
             for (T t : list) {
@@ -104,17 +113,117 @@ public enum MacroProcessor {
             MacroProcessor.writeEnd(outputBuilder, macroBlock);
         }
     },
-    BLOCK {
+    COPY {
+        private record Replace(Pattern pattern, String replacement) {
+        }
+
+        private static IllegalArgumentException illegalReplace(String value) {
+            return new IllegalArgumentException("Illegal replacement pattern: " + value);
+        }
+
+        private static Replace parseReplace(String value) {
+            List<String> list = MacroProcessor.parseStringList(value);
+            if (list.size() != 2)
+                throw illegalReplace(value);
+
+            return new Replace(Pattern.compile(list.get(0)), list.get(1));
+        }
+
         @Override
         public void apply(Document document, Document.MacroBlock macroBlock, StringBuilder outputBuilder) throws IOException {
+            var mutableProperties = new LinkedHashMap<>(macroBlock.properties());
+
+            String name = MacroProcessor.removeSingleProperty(mutableProperties, "NAME");
+            if (name == null)
+                throw new IllegalArgumentException("Missing property: name");
+
+            List<Replace> replaces = Objects.requireNonNullElse(mutableProperties.remove("REPLACE"), List.<String>of())
+                    .stream()
+                    .map(it -> parseReplace(it))
+                    .toList();
+
+            if (!mutableProperties.isEmpty())
+                throw new IllegalArgumentException("Unsupported properties: " + mutableProperties.keySet());
+
+
+            LocalizedDocument localizedDocument = document.directory().getFiles().get(document.name());
+            Document fromDocument;
+            if (localizedDocument == null || (fromDocument = localizedDocument.getDocuments().get(DocumentLocale.ENGLISH)) == null)
+                throw new IOException("Document " + document.name() + " for english does not exist");
+
+            List<String> nameList = List.of(name);
+
+            var fromBlock = (Document.MacroBlock) fromDocument.items().stream()
+                    .filter(it -> it instanceof Document.MacroBlock macro
+                            && macro.name().equals(BLOCK.name())
+                            && nameList.equals(macro.properties().get("NAME"))
+                    )
+                    .findFirst()
+                    .orElseThrow(() -> new IOException("Cannot find the block \"" + name + "\" in " + fromDocument.file()));
+
             MacroProcessor.writeBegin(outputBuilder, macroBlock);
             MacroProcessor.writeProperties(outputBuilder, macroBlock);
-            for (String line : macroBlock.contentLines()) {
+            for (String line : fromBlock.contentLines()) {
+                for (Replace replace : replaces) {
+                    line = replace.pattern.matcher(line).replaceAll(replace.replacement());
+                }
                 outputBuilder.append(line).append('\n');
             }
             MacroProcessor.writeEnd(outputBuilder, macroBlock);
         }
-    };
+    },
+    ;
+
+    private static String removeSingleProperty(Map<String, List<String>> properties, String name) {
+        List<String> values = properties.remove(name);
+        if (values == null || properties.isEmpty())
+            return null;
+
+        if (values.size() != 1)
+            throw new IllegalArgumentException("Unexpected number of property " + name + ": " + values.size());
+
+        return values.get(0);
+    }
+
+    private static List<String> parseStringList(String str) {
+        if (str.isBlank()) {
+            return new ArrayList<>();
+        }
+
+        // Split the string with ' and space cleverly.
+        ArrayList<String> parts = new ArrayList<>(2);
+
+        boolean hasValue = false;
+        StringBuilder current = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); ) {
+            char c = str.charAt(i);
+            if (c == '\'' || c == '"') {
+                hasValue = true;
+                int end = str.indexOf(c, i + 1);
+                if (end < 0) {
+                    end = str.length();
+                }
+                current.append(str, i + 1, end);
+                i = end + 1;
+
+            } else if (c == ' ' || c == '\t') {
+                if (hasValue) {
+                    parts.add(current.toString());
+                    current.setLength(0);
+                    hasValue = false;
+                }
+                i++;
+            } else {
+                hasValue = true;
+                current.append(c);
+                i++;
+            }
+        }
+        if (hasValue)
+            parts.add(current.toString());
+
+        return parts;
+    }
 
     private static void writeBegin(StringBuilder builder, Document.MacroBlock macroBlock) throws IOException {
         builder.append("<!-- #BEGIN ");
