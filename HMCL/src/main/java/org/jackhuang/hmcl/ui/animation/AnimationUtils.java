460aae33e56ef6a2a56f6279948ea6f8b8d9a172
Glavo <zjx001202@gmail.com>
2025-09-15 20:44:23 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/animation/AnimationUtils.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/animation/AnimationUtils.java
@@ -17,7 +17,6 @@
  */
 package org.jackhuang.hmcl.ui.animation;
 
-import org.jackhuang.hmcl.java.JavaRuntime;
 import org.jackhuang.hmcl.setting.ConfigHolder;
 import org.jackhuang.hmcl.ui.FXUtils;
 import org.jackhuang.hmcl.util.platform.OperatingSystem;
@@ -38,10 +37,7 @@ public final class AnimationUtils {
     public static void init() {
     }
 
-    private static final boolean ENABLED = !ConfigHolder.config().isAnimationDisabled()
-            && FXUtils.REDUCED_MOTION != Boolean.TRUE
-            && FXUtils.GPU_ACCELERATION_ENABLED
-            && !JavaRuntime.CURRENT_IN_INTERPRETED_MODE;
+    private static final boolean ENABLED = !ConfigHolder.config().isAnimationDisabled() && FXUtils.REDUCED_MOTION != Boolean.TRUE;
     private static final boolean PLAY_WINDOW_ANIMATION = ENABLED && !OperatingSystem.CURRENT_OS.isLinuxOrBSD();
 
     public static boolean isAnimationEnabled() {
