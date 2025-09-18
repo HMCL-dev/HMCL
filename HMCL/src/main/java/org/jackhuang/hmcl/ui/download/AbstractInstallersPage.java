6a167cfa556e5e8c5bd95fa17d7fbe911b999599
Glavo <zjx001202@gmail.com>
2025-09-18 20:12:12 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/download/AbstractInstallersPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/download/AbstractInstallersPage.java
@@ -120,19 +120,16 @@ public abstract class AbstractInstallersPage extends Control implements WizardPa
                 InstallerItem[] libraries = control.group.getLibraries();
 
                 FlowPane libraryPane = new FlowPane(libraries);
-                libraryPane.setVgap(16);
                 libraryPane.setHgap(16);
 
-                if (libraries.length <= 8) {
-                    BorderPane.setMargin(libraryPane, new Insets(16, 0, 16, 0));
-                    root.setCenter(libraryPane);
-                } else {
-                    ScrollPane scrollPane = new ScrollPane(libraryPane);
-                    scrollPane.setFitToWidth(true);
-                    scrollPane.setFitToHeight(true);
-                    BorderPane.setMargin(scrollPane, new Insets(16, 0, 16, 0));
-                    root.setCenter(scrollPane);
-                }
+                ScrollPane scrollPane = new ScrollPane(libraryPane);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                BorderPane.setMargin(scrollPane, new Insets(8, 0, 8, 0));
+                root.setCenter(scrollPane);
+
+                if (libraries.length <= 8)
+                    scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
             }
 
             {
