ef6cf6f52e7354f5b562be0d2b56af8d0337187a
Glavo <zjx001202@gmail.com>
2025-09-22 17:24:54 +0800

    update

--- buildSrc/src/main/java/org/jackhuang/hmcl/gradle/l10n/CreateLocaleNamesResourceBundle.java
+++ buildSrc/src/main/java/org/jackhuang/hmcl/gradle/l10n/CreateLocaleNamesResourceBundle.java
@@ -125,27 +125,6 @@ public abstract class CreateLocaleNamesResourceBundle extends DefaultTask {
                 throw new GradleException(String.format("File %s already exists", targetFile));
 
             try (var writer = Files.newBufferedWriter(targetFile)) {
-                writer.write("""
-                        #
-                        # Hello Minecraft! Launcher
-                        # Copyright (C) 2025 huangyuhui <huanghongxun2008@126.com> and contributors
-                        #
-                        # This program is free software: you can redistribute it and/or modify
-                        # it under the terms of the GNU General Public License as published by
-                        # the Free Software Foundation, either version 3 of the License, or
-                        # (at your option) any later version.
-                        #
-                        # This program is distributed in the hope that it will be useful,
-                        # but WITHOUT ANY WARRANTY; without even the implied warranty of
-                        # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-                        # GNU General Public License for more details.
-                        #
-                        # You should have received a copy of the GNU General Public License
-                        # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-                        #
-                        
-                        """);
-
                 writer.write("# Languages\n");
                 for (String language : languages) {
                     String displayName = overrideProperties.getProperty(language);
