3900144cd2202210669daedad98f2a85dd0a9314
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-10-01 07:42:54 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModDescriptionTranslatons.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModDescriptionTranslatons.java
@@ -4,29 +4,26 @@ import org.jackhuang.hmcl.mod.RemoteMod;
 import org.jackhuang.hmcl.mod.curse.CurseAddon;
 import org.jackhuang.hmcl.mod.modrinth.ModrinthRemoteModRepository;
 import org.jackhuang.hmcl.task.CacheFileTask;
-import org.jackhuang.hmcl.task.Schedulers;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.util.gson.JsonSerializable;
 import org.jackhuang.hmcl.util.gson.JsonUtils;
 
-import java.nio.file.Path;
+import java.io.IOException;
 
 public final class ModDescriptionTranslatons {
-    private ModDescriptionTranslatons() {}
+    private ModDescriptionTranslatons() {
+    }
 
     public static Task<Response> translate(RemoteMod mod) {
-        return Task.supplyAsync(Schedulers.io(), () -> {
-            String url = null;
-            if (mod.getData() instanceof CurseAddon) {
-                url = "https://mod.mcimirror.top/translate/curseforge/" + mod.getSlug();
-            } else if (mod.getData() instanceof ModrinthRemoteModRepository.ProjectSearchResult project) {
-                url = "https://mod.mcimirror.top/translate/modrinth/" + project.getProjectId();
-            } else if (mod.getData() instanceof ModrinthRemoteModRepository.Project project) {
-                url = "https://mod.mcimirror.top/translate/modrinth/" + project.getId();
-            }
-            Task<Path> cacheFileTask = new CacheFileTask(url);
-            return JsonUtils.fromJsonFile(cacheFileTask.getResult(), Response.class);
-        });
+        String url = null;
+        if (mod.getData() instanceof CurseAddon) {
+            url = "https://mod.mcimirror.top/translate/curseforge/" + mod.getSlug();
+        } else if (mod.getData() instanceof ModrinthRemoteModRepository.ProjectSearchResult project) {
+            url = "https://mod.mcimirror.top/translate/modrinth/" + project.getProjectId();
+        } else if (mod.getData() instanceof ModrinthRemoteModRepository.Project project) {
+            url = "https://mod.mcimirror.top/translate/modrinth/" + project.getId();
+        }
+        return new CacheFileTask(url).thenApplyAsync(path -> JsonUtils.fromJsonFile(path, Response.class));
     }
 
     @JsonSerializable
