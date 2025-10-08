b93cb8958df111577395f0ed6b7418f0efb2bf02
Glavo <zjx001202@gmail.com>
2025-10-08 21:06:33 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModTranslations.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModTranslations.java
@@ -27,7 +27,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 import static org.jackhuang.hmcl.util.Pair.pair;
