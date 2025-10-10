9d2e6eec24b53a87e686c6cd4aef7b9a4321b199
Glavo <zjx001202@gmail.com>
2025-10-10 16:21:56 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/download/VersionsPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/download/VersionsPage.java
@@ -267,8 +267,8 @@ public final class VersionsPage extends Control implements WizardPage, Refreshab
                 }
 
                 switch (NativePatcher.checkSupportedStatus(gameVersion, Platform.SYSTEM_PLATFORM, OperatingSystem.SYSTEM_VERSION)) {
-                    case LAUNCHER_SUPPORTED -> twoLineListItem.addTagWarning(i18n("version.game.support_status.launcher_supported"));
-                    case UNTESTED -> twoLineListItem.addTagWarning(i18n("version.game.support_status.untested"));
+                    case LAUNCHER_SUPPORTED -> twoLineListItem.addTag(i18n("version.game.support_status.launcher_supported"));
+                    case UNTESTED -> twoLineListItem.addTag(i18n("version.game.support_status.untested"));
                     case UNSUPPORTED -> twoLineListItem.addTagWarning(i18n("version.game.support_status.unsupported"));
                 }
             } else {
