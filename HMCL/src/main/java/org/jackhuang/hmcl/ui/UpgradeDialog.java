979218874116d043da1098623683e8892cdb1881
Glavo <zjx001202@gmail.com>
2025-10-19 20:30:56 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/UpgradeDialog.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/UpgradeDialog.java
@@ -22,17 +22,24 @@ import com.jfoenix.controls.JFXDialogLayout;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.ScrollPane;
+import org.jackhuang.hmcl.Metadata;
 import org.jackhuang.hmcl.task.Schedulers;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.ui.construct.DialogCloseEvent;
 import org.jackhuang.hmcl.ui.construct.JFXHyperlink;
 import org.jackhuang.hmcl.upgrade.RemoteVersion;
+import org.jackhuang.hmcl.util.versioning.VersionNumber;
+import org.jetbrains.annotations.Nullable;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.jackhuang.hmcl.Metadata.CHANGELOG_URL;
 import static org.jackhuang.hmcl.ui.FXUtils.onEscPressed;
@@ -40,16 +47,42 @@ import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
 import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 
 public final class UpgradeDialog extends JFXDialogLayout {
+
+    private static final Pattern CHANGELOG_TITLE_PATTERN = Pattern.compile("HMCL (?<version>\\d(?:\\.\\d+)+)");
+
+    private static @Nullable VersionNumber extractVersionNumber(Node node) {
+        String text;
+        if (node instanceof Element element) {
+            text = element.text();
+        } else if (node instanceof TextNode textNode) {
+            text = textNode.text();
+        } else {
+            return null;
+        }
+
+        Matcher matcher = CHANGELOG_TITLE_PATTERN.matcher(text);
+        if (matcher.find())
+            return VersionNumber.asVersion(matcher.group("version"));
+        else
+            return null;
+    }
+
     public UpgradeDialog(RemoteVersion remoteVersion, Runnable updateRunnable) {
         maxWidthProperty().bind(Controllers.getScene().widthProperty().multiply(0.7));
         maxHeightProperty().bind(Controllers.getScene().heightProperty().multiply(0.7));
 
         setHeading(new Label(i18n("update.changelog")));
-        setBody(new ProgressIndicator());
 
         String url = CHANGELOG_URL + remoteVersion.getChannel().channelName + ".html";
         boolean isPreview = remoteVersion.isPreview();
+
+        setBody(new ProgressIndicator());
         Task.supplyAsync(Schedulers.io(), () -> {
+            VersionNumber targetVersion = VersionNumber.asVersion(remoteVersion.getVersion());
+            VersionNumber currentVersion = VersionNumber.asVersion(Metadata.VERSION);
+            if (targetVersion.compareTo(currentVersion) <= 0)
+                throw new IOException("Downgrade update from " + currentVersion + " to " + targetVersion);
+
             Document document = Jsoup.parse(new URL(url), 30 * 1000);
             String id = null;
             Node node = null;
@@ -65,14 +98,24 @@ public final class UpgradeDialog extends JFXDialogLayout {
             if (node == null || !"h1".equals(node.nodeName()))
                 throw new IOException("Cannot find current changelog in document");
 
+            VersionNumber changelogVersion = extractVersionNumber(node);
+            if (changelogVersion == null)
+                throw new IOException("Cannot find current changelog in document. The node: " + node);
+
+            if (!targetVersion.equals(changelogVersion))
+                throw new IOException("The changelog has not been updated yet. Expected: " + targetVersion + ", Actual: " + changelogVersion);
+
             HTMLRenderer renderer = new HTMLRenderer(uri -> {
                 LOG.info("Open link: " + uri);
                 FXUtils.openLink(uri.toString());
             });
 
             do {
-                if ("h1".equals(node.nodeName()) && !id.equals(node.attr("id"))) {
-                    break;
+                if ("h1".equals(node.nodeName())) {
+                    changelogVersion = extractVersionNumber(node);
+                    if (changelogVersion == null || changelogVersion.compareTo(currentVersion) <= 0) {
+                        break;
+                    }
                 }
                 renderer.appendNode(node);
                 node = node.nextSibling();
