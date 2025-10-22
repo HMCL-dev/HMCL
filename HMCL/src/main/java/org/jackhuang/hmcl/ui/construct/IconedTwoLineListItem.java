8cdc9473c91f377d75f7d147451e4989d0253bd4
Glavo <zjx001202@gmail.com>
2025-10-22 21:50:43 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/IconedTwoLineListItem.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/IconedTwoLineListItem.java
@@ -11,7 +11,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -114,17 +113,6 @@ public class IconedTwoLineListItem extends HBox {
             externalLinkButton.getStyleClass().add("toggle-icon4");
             externalLinkButton.setGraphic(SVG.OPEN_IN_NEW.createIcon(Theme.blackFill(), -1));
             externalLinkButton.setOnAction(e -> FXUtils.openLink(externalLink.get()));
-
-            Tooltip tooltip = new Tooltip();
-            FXUtils.onChangeAndOperate(externalLink, link -> {
-                if (StringUtils.isBlank(link)) {
-                    tooltip.setText("");
-                    Tooltip.uninstall(externalLinkButton, tooltip);
-                } else {
-                    tooltip.setText(link);
-                    FXUtils.installFastTooltip(externalLinkButton, tooltip);
-                }
-            });
         }
         return externalLinkButton;
     }
