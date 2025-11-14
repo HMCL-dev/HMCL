2d21ca0580dff4e4da83dbaaaafd65f78082249b
mine_ <mine_yaoym@outlook.com>
2025-11-14 20:49:23 +0800

    fix:修复PR错误

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
@@ -493,7 +493,7 @@ final class ModListPageSkin extends SkinBase<ModListPage> {
                                     }
                                     if (title.getTags()
                                             .stream()
-                                            .noneMatch(it -> it.getText().equals(loaderName))) {
+                                            .noneMatch(it -> it.text().equals(loaderName))) {
                                         title.addTag(loaderName);
                                     }
                                 }
