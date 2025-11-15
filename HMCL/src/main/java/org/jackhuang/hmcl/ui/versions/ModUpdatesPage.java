db2c7e91b729850d16377a649b8caf380131cc77
3gf8jv4dv <3gf8jv4dv@gmail.com>
2025-11-15 18:34:39 +0800

    fix(i18n): improve mod update dialog text
    This commit fixes the text logic for these scenarios:
    
    1. For dialog title and stage text when checking mod updates
    2. For mod update check result page title
    3. For dialog title when updating mods
    
    and adjusts the text content as needed.

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModUpdatesPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModUpdatesPage.java
@@ -111,7 +111,7 @@ public class ModUpdatesPage extends BorderPane implements DecoratorPage {
         JFXButton exportListButton = FXUtils.newRaisedButton(i18n("button.export"));
         exportListButton.setOnAction(e -> exportList());
 
-        JFXButton nextButton = FXUtils.newRaisedButton(i18n("mods.check_updates.update"));
+        JFXButton nextButton = FXUtils.newRaisedButton(i18n("mods.check_updates.confirm"));
         nextButton.setOnAction(e -> updateMods());
 
         JFXButton cancelButton = FXUtils.newRaisedButton(i18n("button.cancel"));
@@ -147,7 +147,7 @@ public class ModUpdatesPage extends BorderPane implements DecoratorPage {
                         Controllers.dialog(i18n("install.success"));
                     }
                 }),
-                i18n("mods.check_updates.update"),
+                i18n("mods.check_updates"),
                 TaskCancellationAction.NORMAL);
     }
 
@@ -276,7 +276,7 @@ public class ModUpdatesPage extends BorderPane implements DecoratorPage {
         private final List<LocalModFile> failedMods = new ArrayList<>();
 
         ModUpdateTask(ModManager modManager, List<Pair<LocalModFile, RemoteMod.Version>> mods) {
-            setStage("mods.check_updates.update");
+            setStage("mods.check_updates.confirm");
             getProperties().put("total", mods.size());
 
             this.dependents = new ArrayList<>();
@@ -308,7 +308,7 @@ public class ModUpdatesPage extends BorderPane implements DecoratorPage {
                                 failedMods.add(local);
                             }
                         })
-                        .withCounter("mods.check_updates.update"));
+                        .withCounter("mods.check_updates.confirm"));
             }
         }
 
