6fa6f23ad4a5d5c15173e5d0970b9c275c18b8d1
Glavo <zjx001202@gmail.com>
2025-11-30 15:40:06 +0800

    在 Manifest 文件中添加 Enable-Final-Field-Mutation 条目 (#4561)

--- HMCL/build.gradle.kts
+++ HMCL/build.gradle.kts
@@ -206,7 +206,8 @@ tasks.shadowJar {
         "Main-Class" to "org.jackhuang.hmcl.Main",
         "Multi-Release" to "true",
         "Add-Opens" to addOpens.joinToString(" "),
-        "Enable-Native-Access" to "ALL-UNNAMED"
+        "Enable-Native-Access" to "ALL-UNNAMED",
+        "Enable-Final-Field-Mutation" to "ALL-UNNAMED",
     )
 
     if (launcherExe.isNotBlank()) {
