2d6e01fcf91afed121253b421ee28fa4a11e51cc
辞庐 <109708109+CiiLu@users.noreply.github.com>
2025-12-14 20:49:23 +0800

    添加输出游戏调试日志选项 (#4144)

--- HMCL/src/main/java/org/jackhuang/hmcl/game/HMCLGameRepository.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/game/HMCLGameRepository.java
@@ -428,6 +428,7 @@ public final class HMCLGameRepository extends DefaultGameRepository {
                 .setNativesDir(vs.getNativesDir())
                 .setProcessPriority(vs.getProcessPriority())
                 .setRenderer(vs.getRenderer())
+                .setEnableDebugLogOutput(vs.isEnableDebugLogOutput())
                 .setUseNativeGLFW(vs.isUseNativeGLFW())
                 .setUseNativeOpenAL(vs.isUseNativeOpenAL())
                 .setDaemon(!makeLaunchScript && vs.getLauncherVisibility().isDaemon())
