71f405b0b598509039f20f63ba3353c921aa692e
Glavo <zjx001202@gmail.com>
2025-12-24 02:15:49 +0800

    update

--- HMCL/src/main/java/com/jfoenix/skins/JFXPopupSkin.java
+++ HMCL/src/main/java/com/jfoenix/skins/JFXPopupSkin.java
@@ -25,17 +25,14 @@ import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXPopup.PopupHPosition;
 import com.jfoenix.controls.JFXPopup.PopupVPosition;
 import com.jfoenix.effects.JFXDepthManager;
-import com.jfoenix.transitions.CacheMomento;
-import com.jfoenix.transitions.CachedTransition;
 import javafx.animation.*;
 import javafx.animation.Animation.Status;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Skin;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.transform.Scale;
 import javafx.util.Duration;
+import org.jackhuang.hmcl.ui.animation.Motion;
 
 /// # Material Design Popup Skin
 /// TODO: REWORK
@@ -57,17 +54,15 @@ public class JFXPopupSkin implements Skin<JFXPopup> {
         this.control = control;
         // set scale y to 0.01 instead of 0 to allow layout of the content,
         // otherwise it will cause exception in traverse engine, when focusing the 1st node
-        scale = new Scale(1, 0.01, 0, 0);
+        scale = new Scale(0, 0, 0, 0);
         popupContent = control.getPopupContent();
         container.getStyleClass().add("jfx-popup-container");
-        container.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
         container.getChildren().add(popupContent);
         container.getTransforms().add(scale);
         container.setOpacity(0);
         root = JFXDepthManager.createMaterialNode(container, 4);
         animation = getAnimation();
     }
-
 
     public void reset(PopupVPosition vAlign, PopupHPosition hAlign, double offsetX, double offsetY) {
         // postion the popup according to its animation
@@ -79,7 +74,8 @@ public class JFXPopupSkin implements Skin<JFXPopup> {
 
     public final void animate() {
         if (animation.getStatus() == Status.STOPPED) {
-            animation.play();
+            container.setOpacity(1);
+            animation.playFromStart();
         }
     }
 
@@ -104,43 +100,30 @@ public class JFXPopupSkin implements Skin<JFXPopup> {
     }
 
     protected Animation getAnimation() {
-        return new PopupTransition();
-    }
+        Interpolator interpolator = Motion.EASE;
 
-    private final class PopupTransition extends CachedTransition {
-        PopupTransition() {
-            super(root, new Timeline(
-                            new KeyFrame(
-                                    Duration.ZERO,
-                                    new KeyValue(popupContent.opacityProperty(), 0, Interpolator.EASE_BOTH),
-                                    new KeyValue(scale.xProperty(), 0, Interpolator.EASE_BOTH),
-                                    new KeyValue(scale.yProperty(), 0, Interpolator.EASE_BOTH)
-                            ),
-                            new KeyFrame(Duration.millis(700),
-                                    new KeyValue(scale.xProperty(), 1, Interpolator.EASE_BOTH),
-                                    new KeyValue(popupContent.opacityProperty(), 0, Interpolator.EASE_BOTH)
-                            ),
-                            new KeyFrame(Duration.millis(1000),
-                                    new KeyValue(popupContent.opacityProperty(), 1, Interpolator.EASE_BOTH),
-                                    new KeyValue(scale.yProperty(), 1, Interpolator.EASE_BOTH)
-                            )
-                    )
-                    , new CacheMomento(popupContent));
-            setCycleDuration(Duration.seconds(.4));
-            setDelay(Duration.seconds(0));
-        }
-
-        @Override
-        protected void starting() {
-            container.setOpacity(1);
-            super.starting();
-        }
+        return new Timeline(
+                new KeyFrame(
+                        Duration.ZERO,
+                        new KeyValue(popupContent.opacityProperty(), 0, interpolator),
+                        new KeyValue(scale.xProperty(), 0, interpolator),
+                        new KeyValue(scale.yProperty(), 0, interpolator)
+                ),
+                new KeyFrame(Duration.millis(280),
+                        new KeyValue(popupContent.opacityProperty(), 0, interpolator),
+                        new KeyValue(scale.xProperty(), 1, interpolator)
+                ),
+                new KeyFrame(Duration.millis(400),
+                        new KeyValue(popupContent.opacityProperty(), 1, interpolator),
+                        new KeyValue(scale.yProperty(), 1, interpolator)
+                )
+        );
     }
 
     public void init() {
         animation.stop();
         container.setOpacity(0);
-        scale.setX(1);
-        scale.setY(0.1);
+        scale.setX(0);
+        scale.setY(0);
     }
 }
