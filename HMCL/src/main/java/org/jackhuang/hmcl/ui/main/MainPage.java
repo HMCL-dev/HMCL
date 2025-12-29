80505fa7ec949230a4e2a8d9ccbbfc51868a0798
neveler <55753029+neveler@users.noreply.github.com>
2025-12-29 12:49:13 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/MainPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/MainPage.java
@@ -39,7 +39,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 import org.jackhuang.hmcl.Metadata;
