786ac7b7ebcd87051c2f890d0bc4283c5b585c51
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-01-01 20:50:11 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPage.java
@@ -46,8 +46,8 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
+import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 
 public final class ModListPage extends ListPageBase<ModListPageSkin.ModInfoObject> implements VersionPage.VersionLoadable, PageAware {
     private final BooleanProperty modded = new SimpleBooleanProperty(this, "modded", false);
@@ -232,7 +232,6 @@ public final class ModListPage extends ListPageBase<ModListPageSkin.ModInfoObjec
     public void checkUpdates() {
         Runnable action = () -> Controllers.taskDialog(Task
                         .composeAsync(() -> {
-                            profile.getVersionSetting(instanceId).setNotCheckGame(true);
                             Optional<String> gameVersion = profile.getRepository().getGameVersion(instanceId);
                             if (gameVersion.isPresent()) {
                                 return new ModCheckUpdatesTask(gameVersion.get(), modManager.getMods());
@@ -251,11 +250,14 @@ public final class ModListPage extends ListPageBase<ModListPageSkin.ModInfoObjec
                         .withStagesHint(Collections.singletonList("update.checking")),
                 i18n("mods.check_updates"), TaskCancellationAction.NORMAL);
 
-        if (!profile.getVersionSetting(instanceId).isNotCheckGame() && profile.getRepository().isModpack(instanceId)) {
+        if (!(profile.getVersionSetting(instanceId).isNotCheckGame()) && profile.getRepository().isModpack(instanceId)) {
             Controllers.confirm(
                     i18n("mods.update_modpack_mod.warning"), null,
                     MessageDialogPane.MessageType.WARNING,
-                    action, null);
+                    () -> {
+                        profile.getVersionSetting(instanceId).setNotCheckGame(true);
+                        action.run();
+                    }, null);
         } else {
             action.run();
         }
