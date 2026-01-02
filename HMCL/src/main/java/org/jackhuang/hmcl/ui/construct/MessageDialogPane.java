581ff577401f0282f8a9560852d636c85592ca21
neveler <55753029+neveler@users.noreply.github.com>
2026-01-02 16:15:27 +0800

    为不同类型的 Dialog 指定相应的默认标题 (#5079)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/MessageDialogPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/MessageDialogPane.java
@@ -98,7 +98,7 @@ public final class MessageDialogPane extends HBox {
         {
             StackPane titlePane = new StackPane();
             titlePane.getStyleClass().addAll("jfx-layout-heading", "title");
-            titlePane.getChildren().setAll(new Label(title != null ? title : i18n("message.info")));
+            titlePane.getChildren().setAll(new Label(title != null ? title : i18n(type.getDisplayName())));
 
             StackPane content = new StackPane();
             content.getStyleClass().add("jfx-layout-body");
