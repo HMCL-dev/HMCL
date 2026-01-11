bd9beb7e8ee562a3b75a73425ea090121708a91f
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-01-11 19:13:28 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModDescriptionTranslation.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModDescriptionTranslation.java
@@ -3,21 +3,28 @@ package org.jackhuang.hmcl.ui.versions;
 import org.jackhuang.hmcl.mod.RemoteMod;
 import org.jackhuang.hmcl.mod.curse.CurseAddon;
 import org.jackhuang.hmcl.mod.modrinth.ModrinthRemoteModRepository;
-import org.jackhuang.hmcl.task.CacheFileTask;
+import org.jackhuang.hmcl.task.GetTask;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.util.gson.JsonSerializable;
-import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.i18n.I18n;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.jackhuang.hmcl.setting.ConfigHolder.config;
 
 public final class ModDescriptionTranslation {
     public static final String API_ROOT = "https://mod.mcimirror.top/translate/";
 
+    private static final Map<RemoteMod, Response> CACHE = new ConcurrentHashMap<>();
+
     private ModDescriptionTranslation() {
     }
 
     public static Task<Response> translate(RemoteMod mod) {
+        if (CACHE.containsKey(mod)) {
+            return Task.completed(CACHE.get(mod));
+        }
         String url;
         if (mod.getData() instanceof CurseAddon) {
             url = API_ROOT + "curseforge/" + mod.getSlug();
@@ -28,7 +35,10 @@ public final class ModDescriptionTranslation {
         } else {
             throw new AssertionError();
         }
-        return new CacheFileTask(url).thenApplyAsync(path -> JsonUtils.fromJsonFile(path, Response.class));
+        return new GetTask(url).thenGetJsonAsync(Response.class).thenApplyAsync(response -> {
+            if (response != null) CACHE.put(mod, response);
+            return response;
+        });
     }
 
     public static boolean enabled() {
