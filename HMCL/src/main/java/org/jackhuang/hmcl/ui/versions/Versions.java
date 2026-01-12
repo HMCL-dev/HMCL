faab41266dd72d32f19656bbe5726f7cc750056e
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-01-12 21:46:43 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/Versions.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/Versions.java
@@ -141,7 +141,7 @@ public final class Versions {
                 reject.accept(i18n("version.manage.rename.fail"));
             }
         }, version, new Validator(i18n("install.new_game.malformed"), HMCLGameRepository::isValidVersionId),
-        new Validator(i18n("install.new_game.already_exists"), newVersionName -> !profile.getRepository().versionIdConflicts(newVersionName) || newVersionName.equals(version)));
+            new Validator(i18n("install.new_game.already_exists"), newVersionName -> !profile.getRepository().versionIdConflicts(newVersionName) || newVersionName.equals(version)));
     }
 
     public static void exportVersion(Profile profile, String version) {
