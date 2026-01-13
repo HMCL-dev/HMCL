ea407be5f0f45579a2c49c132e056054304da8a6
Glavo <zjx001202@gmail.com>
2026-01-13 21:08:36 +0800

    更新 ToolbarListPageSkin 以使用 JFXListView 展示节点 (#5221)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/JavaManagementPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/JavaManagementPage.java
@@ -295,7 +295,7 @@ public final class JavaManagementPage extends ListPageBase<JavaManagementPage.Ja
         }
     }
 
-    private static final class JavaPageSkin extends ToolbarListPageSkin<JavaManagementPage> {
+    private static final class JavaPageSkin extends ToolbarListPageSkin<JavaItem, JavaManagementPage> {
 
         JavaPageSkin(JavaManagementPage skinnable) {
             super(skinnable);
