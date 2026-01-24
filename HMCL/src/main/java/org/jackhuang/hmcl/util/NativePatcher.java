625f62f21406dd7da6231d0786343a6c5baaeb20
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-01-24 13:10:01 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/util/NativePatcher.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/util/NativePatcher.java
@@ -198,9 +198,6 @@ public final class NativePatcher {
     public static SupportStatus checkSupportedStatus(GameVersionNumber gameVersion, Platform platform,
                                                      OSVersion systemVersion) {
         if (platform.equals(Platform.WINDOWS_X86_64)) {
-            if (!systemVersion.isAtLeast(OSVersion.WINDOWS_10) && gameVersion.isAtLeast("1.20.5", "24w14a"))
-                return SupportStatus.UNSUPPORTED;
-
             return SupportStatus.OFFICIAL_SUPPORTED;
         }
 
