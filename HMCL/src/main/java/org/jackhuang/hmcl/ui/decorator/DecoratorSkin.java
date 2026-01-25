3bb7523bc8a1ff2da7b10396c70a4b7feb64a8b4
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-01-25 20:10:53 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorSkin.java
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/ >.
  */
 package org.jackhuang.hmcl.ui.decorator;
 
@@ -137,7 +137,7 @@ public class DecoratorSkin extends SkinBase<Decorator> {
         BorderPane frame = new BorderPane();
         frame.getStyleClass().addAll("jfx-decorator");
         wrapper.getChildren().setAll(frame);
-        skinnable.setDrawerWrapper(wrapper);
+        skinnable.setDrawerWrapper(parent);
 
         parent.getChildren().add(wrapper);
 
