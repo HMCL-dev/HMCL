b8e564fde475d2ea71d65c0e96f9031e0915702f
Glavo <zjx001202@gmail.com>
2026-01-29 20:47:43 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/Versions.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/Versions.java
@@ -290,7 +290,6 @@ public final class Versions {
             Controllers.dialog(dialog);
         } else if (account == null) {
             CreateAccountPane dialog = new CreateAccountPane();
-
             dialog.addEventHandler(DialogCloseEvent.CLOSE, e -> {
                 Account newAccount = Accounts.getSelectedAccount();
                 if (newAccount == null) {
