3801bc10f085bbff8b3f946f6a244d41808ed5e3
Glavo <zjx001202@gmail.com>
2026-02-02 23:52:26 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/terracotta/TerracottaControllerPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/terracotta/TerracottaControllerPage.java
@@ -551,9 +551,9 @@ public class TerracottaControllerPage extends StackPane {
 
         var header = new LinePane();
         header.getStyleClass().add("terracotta-iconed-button");
-        header.setMouseTransparent(true);
         header.setPadding(Insets.EMPTY);
         header.setMinHeight(LinePane.USE_COMPUTED_SIZE);
+        header.setMouseTransparent(true);
         header.setLeftIcon(FXUtils.newBuiltinImage("/assets/img/terracotta.png"));
         header.setTitle(i18n("terracotta.from_local.title"));
         header.setSubtitle(i18n("terracotta.from_local.desc"));
