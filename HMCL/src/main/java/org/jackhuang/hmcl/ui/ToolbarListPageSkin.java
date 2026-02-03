60dafeb8d2afff39d69122fb4295aa7839e79284
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-02-03 22:35:19 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/ToolbarListPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/ToolbarListPageSkin.java
@@ -44,7 +44,9 @@ public abstract class ToolbarListPageSkin<E, P extends ListPageBase<E>> extends 
 
         ComponentList root = new ComponentList();
         root.getStyleClass().add("no-padding");
-        StackPane wrapper = new StackPane(root);
+
+        StackPane container = new StackPane();
+        container.getChildren().add(root);
         StackPane.setMargin(root, new Insets(10));
 
         List<Node> toolbarButtons = initializeToolbar(skinnable);
@@ -56,23 +58,27 @@ public abstract class ToolbarListPageSkin<E, P extends ListPageBase<E>> extends 
             root.getContent().add(toolbar);
         }
 
+        SpinnerPane spinnerPane = new SpinnerPane();
+        spinnerPane.loadingProperty().bind(skinnable.loadingProperty());
+        spinnerPane.failedReasonProperty().bind(skinnable.failedReasonProperty());
+        spinnerPane.onFailedActionProperty().bind(skinnable.onFailedActionProperty());
+        spinnerPane.getStyleClass().add("large-spinner-pane");
+
+        ComponentList.setVgrow(spinnerPane, Priority.ALWAYS);
+
         {
             this.listView = new JFXListView<>();
             this.listView.setPadding(Insets.EMPTY);
             this.listView.setCellFactory(listView -> createListCell((JFXListView<E>) listView));
-            ComponentList.setVgrow(listView, Priority.ALWAYS);
             Bindings.bindContent(this.listView.getItems(), skinnable.itemsProperty());
             FXUtils.ignoreEvent(listView, KeyEvent.KEY_PRESSED, e -> e.getCode() == KeyCode.ESCAPE);
-            SpinnerPane spinnerPane = new SpinnerPane();
+
             spinnerPane.setContent(listView);
-            spinnerPane.loadingProperty().bind(skinnable.loadingProperty());
-            spinnerPane.failedReasonProperty().bind(skinnable.failedReasonProperty());
-            spinnerPane.onFailedActionProperty().bind(skinnable.onFailedActionProperty());
-            spinnerPane.getStyleClass().add("large-spinner-pane");
-            root.getContent().add(spinnerPane);
         }
 
-        getChildren().setAll(wrapper);
+        root.getContent().add(spinnerPane);
+
+        getChildren().setAll(container);
     }
 
     public static Node wrap(Node node) {
