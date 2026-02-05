b7b1dcdfc29c924be767acd1a6db7f6e3f464498
Glavo <zjx001202@gmail.com>
2026-02-05 13:52:26 +0800

    Fix #5437: 修复总任务数为 0 时 Task 进度计算错误的问题

--- HMCLCore/src/main/java/org/jackhuang/hmcl/task/Task.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/task/Task.java
@@ -341,13 +341,20 @@ public abstract class Task<T> {
 
     private long lastUpdateProgressTime = 0L;
 
-    protected void updateProgress(long progress, long total) {
-        updateProgress(1.0 * progress / total);
+    protected void updateProgress(long count, long total) {
+        double progress = (double) count / total;
+        if (progress < 0) {
+            updateProgress(0);
+        } else if (progress > 1.0 || Double.isNaN(progress)) {
+            updateProgress(1.0);
+        } else {
+            updateProgress(progress);
+        }
     }
 
     protected void updateProgress(double progress) {
         if (progress < 0 || progress > 1.0 || Double.isNaN(progress))
-            throw new IllegalArgumentException("Progress must be between 0 and 1.");
+            throw new IllegalArgumentException("Invalid progress: " + progress);
 
         long now = System.currentTimeMillis();
         if (progress == 1.0 || now - lastUpdateProgressTime >= 1000L) {
