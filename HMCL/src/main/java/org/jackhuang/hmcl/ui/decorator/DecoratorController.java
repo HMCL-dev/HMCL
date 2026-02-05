3be586b6e7626551914c813f4e2235235647ce70
Glavo <zjx001202@gmail.com>
2026-02-06 05:43:17 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
@@ -26,6 +26,7 @@ import javafx.beans.InvalidationListener;
 import javafx.beans.WeakInvalidationListener;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
@@ -454,7 +455,7 @@ public class DecoratorController {
     // ==== Toast ====
 
     public void showToast(String content) {
-        decorator.getSnackbar().fireEvent(new JFXSnackbar.SnackbarEvent(content, null, 2000L, false, null));
+        decorator.getSnackbar().fireEvent(new JFXSnackbar.SnackbarEvent(new Label(content), Duration.seconds(2), null));
     }
 
     // ==== Wizard ====
