ded3ff864a0db6a03fee33f9fc539601df33282d
lokins <rnmddos@163.com>
2026-02-07 17:44:42 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/RootPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/RootPage.java
@@ -191,8 +191,7 @@ public class RootPage extends DecoratorAnimatedPage implements DecoratorPage {
             }
 
             AdvancedListItem taskManagerItem = new AdvancedListItem();
-            taskManagerItem.setLeftGraphic(wrap(SVG.LIST)); //TODO SVG待更换
-            taskManagerItem.setActionButtonVisible(false);
+            taskManagerItem.setLeftIcon(SVG.LIST); //TODO SVG待更换
             taskManagerItem.setTitle(i18n("task.manage"));
             taskManagerItem.setOnAction(e -> {
                 Controllers.navigate(new TaskCenterPage());
