d38c7128b463d57e01fd44f28944b24961ba7212
Glavo <zjx001202@gmail.com>
2026-02-17 00:32:29 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/theme/Theme.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/theme/Theme.java
@@ -22,25 +22,9 @@ import org.glavo.monetfx.*;
 import java.util.*;
 
 /// @author Glavo
-public final class Theme {
+public record Theme(ThemeColor primaryColorSeed, Brightness brightness, ColorStyle colorStyle, Contrast contrast) {
 
     public static final Theme DEFAULT = new Theme(ThemeColor.DEFAULT, Brightness.DEFAULT, ColorStyle.FIDELITY, Contrast.DEFAULT);
-
-    private final ThemeColor primaryColorSeed;
-    private final Brightness brightness;
-    private final ColorStyle colorStyle;
-    private final Contrast contrast;
-
-    public Theme(ThemeColor primaryColorSeed,
-                 Brightness brightness,
-                 ColorStyle colorStyle,
-                 Contrast contrast
-    ) {
-        this.primaryColorSeed = primaryColorSeed;
-        this.brightness = brightness;
-        this.colorStyle = colorStyle;
-        this.contrast = contrast;
-    }
 
     public ColorScheme toColorScheme() {
         return ColorScheme.newBuilder()
@@ -51,44 +35,4 @@ public final class Theme {
                 .setContrast(contrast)
                 .build();
     }
-
-    public ThemeColor primaryColorSeed() {
-        return primaryColorSeed;
-    }
-
-    public Brightness brightness() {
-        return brightness;
-    }
-
-    public ColorStyle colorStyle() {
-        return colorStyle;
-    }
-
-    public Contrast contrast() {
-        return contrast;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj == this || obj instanceof Theme that
-                && this.primaryColorSeed.color().equals(that.primaryColorSeed.color())
-                && this.brightness.equals(that.brightness)
-                && this.colorStyle.equals(that.colorStyle)
-                && this.contrast.equals(that.contrast);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(primaryColorSeed, brightness, colorStyle, contrast);
-    }
-
-    @Override
-    public String toString() {
-        return "Theme[" +
-                "primaryColorSeed=" + primaryColorSeed + ", " +
-                "brightness=" + brightness + ", " +
-                "colorStyle=" + colorStyle + ", " +
-                "contrast=" + contrast + ']';
-    }
-
 }
