0bb42b29d047e24b45d8617b986c12e078d0e0e0
Ciilu <109708109+Ciilu@users.noreply.github.com>
2026-02-17 22:42:49 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/GameListPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/GameListPage.java
@@ -1,3 +1,20 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2020  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.ui.versions;
 
 import com.jfoenix.controls.JFXButton;
@@ -82,7 +99,10 @@ public class GameListPage extends DecoratorAnimatedPage implements DecoratorPage
                 pane.setContent(wrapper);
             }
 
-            AdvancedListBox bottomLeftCornerList = new AdvancedListBox().addNavigationDrawerItem(i18n("install.new_game"), SVG.ADD_CIRCLE, Versions::addNewGame).addNavigationDrawerItem(i18n("install.modpack"), SVG.PACKAGE2, Versions::importModpack).addNavigationDrawerItem(i18n("settings.type.global.manage"), SVG.SETTINGS, this::modifyGlobalGameSettings);
+            AdvancedListBox bottomLeftCornerList = new AdvancedListBox()
+                    .addNavigationDrawerItem(i18n("install.new_game"), SVG.ADD_CIRCLE, Versions::addNewGame)
+                    .addNavigationDrawerItem(i18n("install.modpack"), SVG.PACKAGE2, Versions::importModpack)
+                    .addNavigationDrawerItem(i18n("settings.type.global.manage"), SVG.SETTINGS, this::modifyGlobalGameSettings);
             FXUtils.setLimitHeight(bottomLeftCornerList, 40 * 3 + 12 * 2);
             setLeft(pane, bottomLeftCornerList);
         }
