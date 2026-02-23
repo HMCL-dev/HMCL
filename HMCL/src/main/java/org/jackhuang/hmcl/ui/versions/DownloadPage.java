379dceccf1ef0d236ec7f36c1a36f3c2cd1d5a26
Glavo <zjx001202@gmail.com>
2026-02-23 21:58:38 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/DownloadPage.java
@@ -32,7 +32,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Skin;
 import javafx.scene.control.SkinBase;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
