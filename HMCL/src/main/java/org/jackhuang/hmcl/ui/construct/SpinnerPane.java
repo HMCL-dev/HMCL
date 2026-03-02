52a07a9416d66198ad96aa276cde0f29a386d5a6
辞庐 <109708109+Ciilu@users.noreply.github.com>
2026-03-02 12:15:57 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/SpinnerPane.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/SpinnerPane.java
@@ -1,20 +1,3 @@
-/*
- * Hello Minecraft! Launcher
- * Copyright (C) 2020  huangyuhui <huanghongxun2008@126.com> and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package org.jackhuang.hmcl.ui.construct;
 
 import com.jfoenix.controls.JFXSpinner;
@@ -189,6 +172,7 @@ public class SpinnerPane extends Control {
 
         Skin(SpinnerPane control) {
             super(control);
+            root.setClip(null);
 
             updateContent();
             this.getChildren().setAll(root);
@@ -207,6 +191,7 @@ public class SpinnerPane extends Control {
                 if (spinnerPane == null) {
                     spinnerPane = new StackPane(new JFXSpinner());
                     spinnerPane.getStyleClass().add("notice-pane");
+                    spinnerPane.setClip(null);
                 }
                 nextContent = spinnerPane;
             } else if (control.getFailedReason() != null) {
@@ -214,6 +199,7 @@ public class SpinnerPane extends Control {
                     failedReasonLabel = new Label();
                     failedPane = new StackPane(failedReasonLabel);
                     failedPane.getStyleClass().add("notice-pane");
+                    failedPane.setClip(null);
                     FXUtils.onClicked(failedPane, () -> control.fireEvent(new Event(SpinnerPane.FAILED_ACTION)));
                 }
                 failedReasonLabel.setText(control.getFailedReason());
@@ -221,6 +207,7 @@ public class SpinnerPane extends Control {
             } else {
                 if (contentPane == null) {
                     contentPane = new StackPane();
+                    contentPane.setClip(null);
                 }
 
                 Node content = control.getContent();
