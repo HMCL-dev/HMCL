15cc754f6fc15e1a299a03175db230e5a691676b
Glavo <zjx001202@gmail.com>
2026-03-13 21:56:35 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorController.java
@@ -58,7 +58,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.stream.Collectors;
