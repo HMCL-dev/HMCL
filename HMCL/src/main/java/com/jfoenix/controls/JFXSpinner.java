772378d49d046d436a065551d0e826b5de5e586c
Glavo <zjx001202@gmail.com>
2026-03-18 13:09:01 +0800

    Remove unused RandomGenerator import

--- HMCL/src/main/java/com/jfoenix/controls/JFXSpinner.java
+++ HMCL/src/main/java/com/jfoenix/controls/JFXSpinner.java
@@ -32,7 +32,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.random.RandomGenerator;
 
 /// JFXSpinner is the material design implementation of a loading spinner.
 ///
