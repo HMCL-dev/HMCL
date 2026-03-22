9aa867f9dcbcb5e1983d9507cc2eaae5b49650aa
Calboot <calboot39@outlook.com>
2026-03-22 16:05:08 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/NBTUtils.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/NBTUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2026 huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.jackhuang.hmcl.util;
 
 import org.glavo.nbt.io.NBTCodec;
@@ -32,6 +49,9 @@ public final class NBTUtils {
 
     public static @Nullable String tryGetString(Tag tag) {
         return tag instanceof StringTag ? ((StringTag) tag).getValue() : null;
+    }
+
+    private NBTUtils() {
     }
 
 }
