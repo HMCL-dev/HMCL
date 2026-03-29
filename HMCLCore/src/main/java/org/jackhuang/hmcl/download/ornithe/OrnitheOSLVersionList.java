413815d61077ffe61f929ae52524a4f2051cb3d9
辞庐 <109708109+Ciilu@users.noreply.github.com>
2026-03-29 09:13:17 +0800

    月影弯弯 勾住了夜晚 浮幻 刻下浪漫 倒映多少萤光伴我清歌共枕眠 唤起谁的思绪 谁的眷恋

---
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/ornithe/OrnitheOSLVersionList.java
@@ -0,0 +1,53 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2021  huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.jackhuang.hmcl.download.ornithe;
+
+import org.jackhuang.hmcl.download.DownloadProvider;
+import org.jackhuang.hmcl.download.VersionList;
+import org.jackhuang.hmcl.mod.RemoteMod;
+import org.jackhuang.hmcl.mod.modrinth.ModrinthRemoteModRepository;
+import org.jackhuang.hmcl.task.Task;
+import org.jackhuang.hmcl.util.Lang;
+
+import java.util.Collections;
+
+public class OrnitheOSLVersionList extends VersionList<OrnitheOSLRemoteVersion> {
+
+    private final DownloadProvider downloadProvider;
+
+    public OrnitheOSLVersionList(DownloadProvider downloadProvider) {
+        this.downloadProvider = downloadProvider;
+    }
+
+    @Override
+    public boolean hasType() {
+        return false;
+    }
+
+    @Override
+    public Task<?> refreshAsync() {
+        return Task.runAsync(() -> {
+            for (RemoteMod.Version modVersion : Lang.toIterable(ModrinthRemoteModRepository.MODS.getRemoteVersionsById(downloadProvider, "P7dR8mSH"))) {
+                for (String gameVersion : modVersion.getGameVersions()) {
+                    versions.put(gameVersion, new OrnitheOSLRemoteVersion(gameVersion, modVersion.getVersion(), modVersion.getName(), modVersion.getDatePublished(), modVersion,
+                            Collections.singletonList(modVersion.getFile().getUrl())));
+                }
+            }
+        });
+    }
+}
