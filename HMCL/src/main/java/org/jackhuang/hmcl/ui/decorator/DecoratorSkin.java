2dad1bab7960d3cd40f066c8b9310d42c77c33eb
辞庐 <109708109+Ciilu@users.noreply.github.com>
2026-04-05 22:44:00 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/decorator/DecoratorSkin.java
@@ -274,6 +274,7 @@ public class DecoratorSkin extends SkinBase<Decorator> {
 
             if (canBack) {
                 JFXButton backNavButton = new JFXButton();
+                skinnable.forbidDraggingWindow(backNavButton);
                 backNavButton.setFocusTraversable(false);
                 backNavButton.setGraphic(SVG.ARROW_BACK.createIcon(Themes.titleFillProperty()));
                 backNavButton.getStyleClass().add("jfx-decorator-button");
@@ -285,6 +286,7 @@ public class DecoratorSkin extends SkinBase<Decorator> {
 
             if (canClose) {
                 JFXButton closeNavButton = new JFXButton();
+                skinnable.forbidDraggingWindow(closeNavButton);
                 closeNavButton.setFocusTraversable(false);
                 closeNavButton.setGraphic(SVG.CLOSE.createIcon(Themes.titleFillProperty()));
                 closeNavButton.getStyleClass().add("jfx-decorator-button");
@@ -300,8 +302,6 @@ public class DecoratorSkin extends SkinBase<Decorator> {
             if (canBack || canClose) {
                 navBar.setLeft(navLeft);
             }
-
-            skinnable.forbidDraggingWindow(navLeft);
 
             BorderPane center = new BorderPane();
             if (title != null) {
@@ -351,6 +351,7 @@ public class DecoratorSkin extends SkinBase<Decorator> {
                 refreshNavButton.setGraphic(SVG.REFRESH.createIcon(Themes.titleFillProperty()));
                 refreshNavButton.getStyleClass().add("jfx-decorator-button");
                 refreshNavButton.onActionProperty().bind(skinnable.onRefreshNavButtonActionProperty());
+                skinnable.forbidDraggingWindow(refreshNavButton);
 
                 Rectangle separator = new Rectangle();
                 separator.visibleProperty().bind(refreshNavButton.visibleProperty());
