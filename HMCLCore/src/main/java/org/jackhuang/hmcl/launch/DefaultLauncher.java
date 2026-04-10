4ea9843b80d262569cf702067c521741d4412342
Glavo <zjx001202@gmail.com>
2026-04-10 15:09:36 +0800

    refactor: remove unused JavaRuntime import in DefaultLauncher.java

--- HMCLCore/src/main/java/org/jackhuang/hmcl/launch/DefaultLauncher.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/launch/DefaultLauncher.java
@@ -20,7 +20,6 @@ package org.jackhuang.hmcl.launch;
 import org.jackhuang.hmcl.auth.AuthInfo;
 import org.jackhuang.hmcl.download.LibraryAnalyzer;
 import org.jackhuang.hmcl.game.*;
-import org.jackhuang.hmcl.java.JavaRuntime;
 import org.jackhuang.hmcl.util.Lang;
 import org.jackhuang.hmcl.util.ServerAddress;
 import org.jackhuang.hmcl.util.StringUtils;
