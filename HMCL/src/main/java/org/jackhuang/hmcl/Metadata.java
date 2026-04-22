595ee46021bbb8920c58085b20aa141ff6bd2c6f
Glavo <zjx001202@gmail.com>
2026-04-22 21:08:09 +0800

    更新配置目录环境变量名称为 `HMCL_LOCAL_HOME` 以替代 `HMCL_DATA_DIR`

--- HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
@@ -82,7 +82,7 @@ public final class Metadata {
             HMCL_GLOBAL_DIRECTORY = Path.of(hmclHome).toAbsolutePath().normalize();
         }
 
-        String hmclCurrentDir = System.getProperty("hmcl.dir", System.getenv("HMCL_DATA_DIR"));
+        String hmclCurrentDir = System.getProperty("hmcl.dir", System.getenv("HMCL_LOCAL_HOME"));
         HMCL_CURRENT_DIRECTORY = StringUtils.isNotBlank(hmclCurrentDir)
                 ? Path.of(hmclCurrentDir).toAbsolutePath().normalize()
                 : CURRENT_DIRECTORY.resolve(".hmcl");
