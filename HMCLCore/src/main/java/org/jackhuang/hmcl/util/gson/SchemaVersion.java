5ad36b95e37d535572c2535cfbe3b62620f4a6d4
Glavo <zjx001202@gmail.com>
2026-05-24 19:29:58 +0800

    Move legacy preset migration into LegacyConfigMigrator

---
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/gson/SchemaVersion.java
@@ -0,0 +1,78 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2026 huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.jackhuang.hmcl.util.gson;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+
+/// @author Glavo
+@JsonSerializable
+@JsonAdapter(SchemaVersion.Adapter.class)
+@NotNullByDefault
+public record SchemaVersion(int major, int minor) {
+
+    @Override
+    public String toString() {
+        return major + "." + minor;
+    }
+
+    @NotNullByDefault
+    public static final class Adapter extends TypeAdapter<@Nullable SchemaVersion> {
+        @Override
+        public void write(JsonWriter out, @Nullable SchemaVersion value) throws IOException {
+            if (value != null) {
+                out.value(value.toString());
+            } else {
+                out.nullValue();
+            }
+        }
+
+        @Override
+        public @Nullable SchemaVersion read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            if (in.peek() == JsonToken.STRING) {
+                String version = in.nextString();
+                int dot = version.indexOf('.');
+
+                try {
+                    if (dot == -1) {
+                        return new SchemaVersion(Integer.parseInt(version), 0);
+                    } else {
+                        return new SchemaVersion(Integer.parseInt(version.substring(0, dot)), Integer.parseInt(version.substring(dot + 1)));
+                    }
+                } catch (NumberFormatException e) {
+                    throw new JsonParseException("Invalid schema version: " + version, e);
+                }
+            } else {
+                throw new JsonParseException("Unexpected token: " + in.peek());
+            }
+        }
+    }
+}
