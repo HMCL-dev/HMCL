48789faa6343f01be7aefe23e76b110f692b63c6
Glavo <zjx001202@gmail.com>
2026-06-17 20:18:11 +0800

    fix checkstyle

--- HMCL/src/main/java/org/jackhuang/hmcl/setting/LegacyConfigMigrator.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/setting/LegacyConfigMigrator.java
@@ -39,7 +39,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
--- HMCL/src/main/java/org/jackhuang/hmcl/setting/SettingsManager.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/setting/SettingsManager.java
@@ -40,7 +40,6 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.IdentityHashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
