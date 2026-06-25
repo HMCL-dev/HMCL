cdee95fdbca961fbceff1878a755f3b41fe162c6
Alvaro842DEV <139498211+Alvaro842DEV@users.noreply.github.com>
2026-06-25 16:25:17 +0200

    feat: cambiar nombre a Infernum Launcher

--- HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
@@ -33,9 +33,9 @@ public final class Metadata {
     private Metadata() {
     }
 
-    public static final String NAME = "HMCL";
-    public static final String FULL_NAME = "Hello Minecraft! Launcher";
-    public static final String VERSION = System.getProperty("hmcl.version.override", JarUtils.getAttribute("hmcl.version", "@develop@"));
+    public static final String NAME = "Infernum Launcher";
+    public static final String FULL_NAME = "Infernum Launcher";
+    public static final String VERSION = System.getProperty("hmcl.version.override", JarUtils.getAttribute("hmcl.version", "1.0"));
 
     public static final String TITLE = NAME + " " + VERSION;
     public static final String FULL_TITLE = FULL_NAME + " v" + VERSION;
