1d35f49af88c318121fb0dd1900c629f38c8e008
快乐凤爪王 <165921128+KSSJW@users.noreply.github.com>
2026-06-26 21:03:18 +0800

    Fix: Fix the alignment issue of the icon in TerracottaControllerPage. (#6227)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/terracotta/TerracottaControllerPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/terracotta/TerracottaControllerPage.java
@@ -32,6 +32,8 @@ import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextFlow;
@@ -554,6 +556,7 @@ public class TerracottaControllerPage extends StackPane {
         header.getStyleClass().add("no-padding");
         header.setLargeTitle(true);
         header.setMinHeight(LinePane.USE_COMPUTED_SIZE);
+        HBox.setHgrow(header, Priority.ALWAYS);
         header.setMouseTransparent(true);
         header.setLeading(FXUtils.newBuiltinImage("/assets/img/terracotta.png"));
         header.setTitle(i18n("terracotta.from_local.title"));
