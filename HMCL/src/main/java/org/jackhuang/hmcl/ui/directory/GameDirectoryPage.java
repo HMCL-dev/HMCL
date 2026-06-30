79626cffa0ef7d3028a4a4fdf4c46a62900e30de
Glavo <zjx001202@gmail.com>
2026-06-30 15:10:24 +0800

    refactor: rename package from gamedirectory to directory in related components

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/directory/GameDirectoryPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/directory/GameDirectoryPage.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.jackhuang.hmcl.ui.gamedirectory;
+package org.jackhuang.hmcl.ui.directory;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
