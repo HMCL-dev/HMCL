1c271197178737c6a83e5f03fe4909bee8f80f02
辞庐 <109708109+CiiLu@users.noreply.github.com>
2026-07-05 21:09:00 +0800

    煞见祸绝断，令风雨也同甘，佑尔国泰惠泽民安。

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/account/FriendListPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/account/FriendListPage.java
@@ -17,16 +17,140 @@
  */
 package org.jackhuang.hmcl.ui.account;
 
-import javafx.scene.layout.Region;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.Skin;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import org.jackhuang.hmcl.auth.Account;
 import org.jackhuang.hmcl.game.friend.FriendControl;
+import org.jackhuang.hmcl.task.Schedulers;
+import org.jackhuang.hmcl.task.Task;
+import org.jackhuang.hmcl.ui.FXUtils;
+import org.jackhuang.hmcl.ui.ListPageBase;
+import org.jackhuang.hmcl.ui.SVG;
+import org.jackhuang.hmcl.ui.ToolbarListPageSkin;
+import org.jackhuang.hmcl.ui.animation.TransitionPane;
+import org.jackhuang.hmcl.ui.construct.AdvancedListBox;
+import org.jackhuang.hmcl.ui.construct.TabHeader;
+import org.jackhuang.hmcl.ui.decorator.DecoratorAnimatedPage;
+import org.jackhuang.hmcl.ui.decorator.DecoratorPage;
 
-public class FriendListPage extends Region {
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.jackhuang.hmcl.util.i18n.I18n.i18n;
+import static org.jackhuang.hmcl.util.logging.Logger.LOG;
+
+public final class FriendListPage extends DecoratorAnimatedPage implements DecoratorPage {
     private final Account account;
     private final FriendControl control;
+
+    private final TabHeader tab;
+    private final TabHeader.Tab<Right> friendPage = new TabHeader.Tab<>("friendPage");
+    private final TransitionPane transitionPane = new TransitionPane();
+
+    @SuppressWarnings("unused")
+    private ChangeListener<String> instanceChangeListenerHolder;
+
+
+    private ReadOnlyObjectWrapper<State> state = null;
 
     public FriendListPage(Account account, FriendControl control) {
         this.account = account;
         this.control = control;
+
+        friendPage.setNodeSupplier(Right::new);
+        tab = new TabHeader(transitionPane, friendPage);
+        tab.select(friendPage);
+
+        BorderPane left = new BorderPane();
+        FXUtils.setLimitWidth(left, 200);
+        VBox.setVgrow(left, Priority.ALWAYS);
+        setLeft(left);
+
+        AdvancedListBox sideBar = new AdvancedListBox().addNavigationDrawerTab(tab, friendPage, i18n("account.friend"), SVG.GROUP);
+        left.setTop(sideBar);
+
+        AdvancedListBox toolbar = new AdvancedListBox().addNavigationDrawerItem(i18n("account.friend.add"), SVG.PERSON_ADD, this::onAddFriend);
+        BorderPane.setMargin(toolbar, new Insets(0, 0, 12, 0));
+        left.setBottom(toolbar);
+
+        setCenter(transitionPane);
+    }
+
+    private void onAddFriend() {
+
+    }
+
+    @Override
+    public ReadOnlyObjectProperty<State> stateProperty() {
+        if (state == null)
+            state = new ReadOnlyObjectWrapper<>(State.fromTitle(i18n("account.friend") + " - " + account.getProfileName()));
+        return state;
+    }
+
+    private final class Right extends ListPageBase<FriendListItem> {
+        private Right() {
+            super();
+
+            refresh();
+        }
+
+        private void refresh() {
+            setItems(FXCollections.emptyObservableList());
+            setLoading(true);
+            setFailedReason(null);
+
+            Task.supplyAsync(control::getFriendList).whenComplete(Schedulers.javafx(), (result, exception) -> {
+                setLoading(false);
+                if (exception != null) {
+                    LOG.warning("Failed to get friend list: " + exception);
+                    setFailedReason(i18n("account.friend.failed"));
+                } else {
+                    getItems().addAll(result.friends().stream().map(it -> new FriendListItem(it.profileId(), it.name(), FriendStatus.FRIEND)).collect(Collectors.toList()));
+                    getItems().addAll(result.outgoingRequests().stream().map(it -> new FriendListItem(it.profileId(), it.name(), FriendStatus.OUTGOING)).collect(Collectors.toList()));
+                    getItems().addAll(result.incomingRequests().stream().map(it -> new FriendListItem(it.profileId(), it.name(), FriendStatus.INCOMING)).collect(Collectors.toList()));
+                }
+
+            }).start();
+        }
+
+        @Override
+        protected Skin<?> createDefaultSkin() {
+            return new RightSkin(this);
+        }
+    }
+
+    private final class RightSkin extends ToolbarListPageSkin<FriendListItem, Right> {
+        public RightSkin(Right skinnable) {
+            super(skinnable);
+
+            listView.setCellFactory(x -> new FriendListCell());
+        }
+
+        @Override
+        protected List<Node> initializeToolbar(Right skinnable) {
+            return List.of();
+        }
+    }
+
+    private final class FriendListCell extends ListCell<FriendListItem> {
+
+    }
+
+    private record FriendListItem(String profileId, String name, FriendStatus status) {
+    }
+
+    private enum FriendStatus {
+        FRIEND,
+        INCOMING,
+        OUTGOING
     }
 }
