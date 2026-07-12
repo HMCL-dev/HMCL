ce7915d30eeea8cd65f23e8e51fe37d49d458127
Glavo <zjx001202@gmail.com>
2026-07-12 22:14:40 +0800

    Refactor: Simplify background size handling in LauncherBackground

--- HMCL/src/main/java/org/jackhuang/hmcl/theme/Themes.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/theme/Themes.java
@@ -945,8 +945,7 @@ public final class Themes {
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.DEFAULT,
-                new BackgroundSize(800, 480, false, false, true, true)
-        ));
+                BackgroundSize.DEFAULT));
         return new LauncherBackground(imageBackground, MathUtils.clamp(opacity, 0., 1.));
     }
 
