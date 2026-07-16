72c7b84809834679198d41a641ccee22b8e0f081
Zkitefly <zkitefly@foxmail.com>
2026-07-16 17:35:15 +0800

    fix

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPage.java
@@ -60,7 +60,6 @@ import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
