0e48539cd4ec528a713defbacbd62e185a642c38
xirren <xirren@outlook.com>
2026-07-27 21:08:04 +0800

    fix: remove unused SettingsManager.state import

--- HMCL/src/main/java/org/jackhuang/hmcl/game/LauncherHelper.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/game/LauncherHelper.java
@@ -69,7 +69,6 @@ import java.util.stream.Collectors;
 
 import static javafx.application.Platform.runLater;
 import static javafx.application.Platform.setImplicitExit;
-import static org.jackhuang.hmcl.setting.SettingsManager.state;
 import static org.jackhuang.hmcl.ui.FXUtils.runInFX;
 import static org.jackhuang.hmcl.util.DataSizeUnit.MEGABYTES;
 import static org.jackhuang.hmcl.util.Lang.resolveException;
