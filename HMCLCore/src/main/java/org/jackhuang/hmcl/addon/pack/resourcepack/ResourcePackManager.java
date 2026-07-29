3941a808a142ee7214d56d9ad5872ab020dede7f
ToobLac <tooblac39@gmail.com>
2026-07-29 21:53:09 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/addon/pack/resourcepack/ResourcePackManager.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/addon/pack/resourcepack/ResourcePackManager.java
@@ -517,6 +517,7 @@ public final class ResourcePackManager extends LocalAddonManager<ResourcePackFil
 
     public boolean rename(String oldName, String newName) {
         String oldNameNewFormat = "file/" + oldName;
+        String newNameNewFormat = "file/" + newName;
         boolean modified = false;
 
         lock.lock();
@@ -529,7 +530,7 @@ public final class ResourcePackManager extends LocalAddonManager<ResourcePackFil
                 while (it.hasNext()) {
                     var next = it.next();
                     if (oldName.equals(next) || oldNameNewFormat.equals(next)) {
-                        it.set(newName);
+                        it.set(supportsNewOptionsFormat ? newNameNewFormat : newName);
                         modified = true;
                     }
                 }
