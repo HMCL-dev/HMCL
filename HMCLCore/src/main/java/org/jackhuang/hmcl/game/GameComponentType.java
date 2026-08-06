0b6a129a0909dea0ef1ebae8de7aa409da3b6916
Glavo <zjx001202@gmail.com>
2026-08-06 22:09:34 +0800

    feat(GameComponentType): add enum for game component types and library matching logic

---
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/game/GameComponentType.java
@@ -0,0 +1,263 @@
+/*
+ * Hello Minecraft! Launcher
+ * Copyright (C) 2026 huangyuhui <huanghongxun2008@126.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.jackhuang.hmcl.game;
+
+import org.jackhuang.hmcl.addon.mod.ModLoaderType;
+import org.jetbrains.annotations.NotNullByDefault;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/// @author Glavo
+@NotNullByDefault
+public enum GameComponentType {
+    GAME("game", null) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return true;
+        }
+    },
+    LEGACY_FABRIC("legacyfabric", ModLoaderType.LEGACY_FABRIC) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            if ("net.fabricmc".equals(library.groupId()) && "fabric-loader".equals(library.artifactId())) {
+                for (Library l : libraries) {
+                    if ("net.legacyfabric".equals(l.groupId())) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    },
+    LEGACY_FABRIC_API("legacyfabric-api", null) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "net.legacyfabric".equals(library.groupId()) && "legacyfabric-api".equals(library.artifactId());
+        }
+    },
+    FABRIC("fabric", ModLoaderType.FABRIC) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            if ("net.fabricmc".equals(library.groupId()) && "fabric-loader".equals(library.artifactId())) {
+                for (Library l : libraries) {
+                    if ("net.legacyfabric".equals(l.groupId())) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    },
+    FABRIC_API("fabric-api", null) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "net.fabricmc".equals(library.groupId()) && "fabric-api".equals(library.artifactId());
+        }
+    },
+    FORGE("forge", ModLoaderType.FORGE) {
+        private final Pattern FORGE_VERSION_MATCHER = Pattern.compile("^([0-9.]+)-(?<forge>[0-9.]+)(-([0-9.]+))?$");
+
+        @Override
+        protected @Nullable String patchVersion(GameInstanceManifest manifest, String libraryVersion) {
+            Matcher matcher = FORGE_VERSION_MATCHER.matcher(libraryVersion);
+            if (matcher.find()) {
+                return matcher.group("forge");
+            }
+            return super.patchVersion(manifest, libraryVersion);
+        }
+
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            for (Library l : libraries) {
+                if (NEO_FORGE.matchLibrary(l, libraries)) {
+                    return false;
+                }
+            }
+
+            return "net.minecraftforge".equals(library.groupId()) && ("forge".equals(library.artifactId()) || "fmlloader".equals(library.artifactId()));
+        }
+    },
+    CLEANROOM("cleanroom", ModLoaderType.CLEANROOM) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "com.cleanroommc".equals(library.groupId()) && "cleanroom".equals(library.artifactId());
+        }
+    },
+    NEO_FORGE("neoforge", ModLoaderType.NEO_FORGE) {
+        private final Pattern NEO_FORGE_VERSION_MATCHER = Pattern.compile("^([0-9.]+)-(?<forge>[0-9.]+)(-([0-9.]+))?$");
+
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "net.neoforged.fancymodloader".equals(library.groupId()) && ("core".equals(library.artifactId()) || "loader".equals(library.artifactId()));
+        }
+
+        @Override
+        protected @Nullable String patchVersion(GameInstanceManifest manifest, String libraryVersion) {
+            String res = scanVersion(manifest);
+            if (res != null) {
+                return res;
+            }
+
+            for (GameInstancePatch patch : manifest.getPatches()) {
+                res = scanPatch(patch);
+                if (res != null) {
+                    return res;
+                }
+            }
+
+            Matcher matcher = NEO_FORGE_VERSION_MATCHER.matcher(libraryVersion);
+            if (matcher.find()) {
+                return matcher.group("forge");
+            }
+
+            return libraryVersion;
+        }
+
+        private @Nullable String scanVersion(GameInstanceManifest manifest) {
+            if (manifest.arguments() == null) {
+                return null;
+            }
+            List<Argument> gameArguments = manifest.arguments().game();
+            if (gameArguments == null) {
+                return null;
+            }
+
+            for (int i = 0; i < gameArguments.size() - 1; i++) {
+                Argument argument = gameArguments.get(i);
+                if (argument instanceof StringArgument) {
+                    String argumentValue = ((StringArgument) argument).argument();
+                    if ("--fml.neoForgeVersion".equals(argumentValue) || "--fml.forgeVersion".equals(argumentValue)) {
+                        Argument next = gameArguments.get(i + 1);
+                        if (next instanceof StringArgument) {
+                            return ((StringArgument) next).argument();
+                        }
+                        return null; // Normally, there should not be two --fml.neoForgeVersion argument.
+                    }
+                }
+            }
+            return null;
+        }
+
+        private @Nullable String scanPatch(GameInstancePatch patch) {
+            Arguments optArgument = patch.arguments();
+            if (optArgument == null) {
+                return null;
+            }
+            List<Argument> gameArguments = optArgument.game();
+            if (gameArguments == null) {
+                return null;
+            }
+
+            for (int i = 0; i < gameArguments.size() - 1; i++) {
+                Argument argument = gameArguments.get(i);
+                if (argument instanceof StringArgument) {
+                    String argumentValue = ((StringArgument) argument).argument();
+                    if ("--fml.neoForgeVersion".equals(argumentValue) || "--fml.forgeVersion".equals(argumentValue)) {
+                        Argument next = gameArguments.get(i + 1);
+                        if (next instanceof StringArgument) {
+                            return ((StringArgument) next).argument();
+                        }
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+    },
+    LITELOADER("liteloader", ModLoaderType.LITE_LOADER) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "com.mumfrey".equals(library.groupId()) && "liteloader".equals(library.artifactId());
+        }
+    },
+    OPTIFINE("optifine", null) {
+        private static final Set<String> GROUPS = Set.of("net.optifine", "optifine");
+
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return GROUPS.contains(library.groupId()) && !library.artifactId().contains("launchwrapper");
+        }
+    },
+    QUILT("quilt", ModLoaderType.QUILT) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "org.quiltmc".equals(library.groupId()) && "quilt-loader".equals(library.artifactId());
+        }
+    },
+    QUILT_API("quilt-api", null) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "org.quiltmc".equals(library.groupId()) && "quilt-api".equals(library.artifactId());
+        }
+    },
+    BOOTSTRAP_LAUNCHER("", null) {
+        @Override
+        protected boolean matchLibrary(Library library, List<Library> libraries) {
+            return "cpw.mods".equals(library.groupId()) && "bootstraplauncher".equals(library.artifactId());
+        }
+    };
+
+    private final String patchId;
+    private final @Nullable ModLoaderType modLoaderType;
+
+    private static final Map<String, GameComponentType> PATCH_ID_MAP = new HashMap<>();
+
+    static {
+        for (GameComponentType type : values()) {
+            PATCH_ID_MAP.put(type.getPatchId(), type);
+        }
+    }
+
+    GameComponentType(String patchId, @Nullable ModLoaderType modLoaderType) {
+        this.patchId = patchId;
+        this.modLoaderType = modLoaderType;
+    }
+
+    public boolean isModLoader() {
+        return modLoaderType != null;
+    }
+
+    public String getPatchId() {
+        return patchId;
+    }
+
+    public @Nullable ModLoaderType getModLoaderType() {
+        return modLoaderType;
+    }
+
+    public static @Nullable GameComponentType fromPatchId(String patchId) {
+        return PATCH_ID_MAP.get(patchId);
+    }
+
+    protected abstract boolean matchLibrary(Library library, List<Library> libraries);
+
+    protected @Nullable String patchVersion(GameInstanceManifest manifest, String libraryVersion) {
+        return libraryVersion;
+    }
+
+}
