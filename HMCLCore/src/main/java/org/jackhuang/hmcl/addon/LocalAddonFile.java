8100fd04c9c265896dd9b7ce3426414d6dd52907
ToobLac <tooblac39@gmail.com>
2026-08-06 11:34:04 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/addon/LocalAddonFile.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/addon/LocalAddonFile.java
@@ -21,7 +21,6 @@ import org.jackhuang.hmcl.download.DownloadProvider;
 import org.jackhuang.hmcl.util.Pair;
 import org.jackhuang.hmcl.util.StringUtils;
 import org.jackhuang.hmcl.util.io.FileUtils;
-import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
