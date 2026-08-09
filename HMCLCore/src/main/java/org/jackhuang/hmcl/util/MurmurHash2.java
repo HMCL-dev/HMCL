e7596d726f49b6d47b117495927f3ed45fdb7964
Glavo <zjx001202@gmail.com>
2026-08-09 21:21:23 +0800

    refactor: remove unused string hash methods from MurmurHash2

--- HMCLCore/src/main/java/org/jackhuang/hmcl/util/MurmurHash2.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/util/MurmurHash2.java
@@ -17,8 +17,6 @@
  */
 package org.jackhuang.hmcl.util;
 
-import java.nio.charset.StandardCharsets;
-
 /**
  * Implementation of the MurmurHash2 32-bit and 64-bit hash functions.
  *
@@ -132,50 +130,6 @@ public final class MurmurHash2 {
     }
 
     /**
-     * Generates a 32-bit hash from a string with a default seed.
-     * <p>
-     * Before 1.14 the string was converted using default encoding.
-     * Since 1.14 the string is converted to bytes using UTF-8 encoding.
-     * </p>
-     * This is a helper method that will produce the same result as:
-     *
-     * <pre>
-     * int seed = 0x9747b28c;
-     * byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-     * int hash = MurmurHash2.hash32(bytes, bytes.length, seed);
-     * </pre>
-     *
-     * @param text The input string
-     * @return The 32-bit hash
-     * @see #hash32(byte[], int, int)
-     */
-    public static int hash32(final String text) {
-        final byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-        return hash32(bytes, bytes.length);
-    }
-
-    /**
-     * Generates a 32-bit hash from a substring with a default seed value.
-     * The string is converted to bytes using the default encoding.
-     * This is a helper method that will produce the same result as:
-     *
-     * <pre>
-     * int seed = 0x9747b28c;
-     * byte[] bytes = text.substring(from, from + length).getBytes(StandardCharsets.UTF_8);
-     * int hash = MurmurHash2.hash32(bytes, bytes.length, seed);
-     * </pre>
-     *
-     * @param text   The input string
-     * @param from   The starting index
-     * @param length The length of the substring
-     * @return The 32-bit hash
-     * @see #hash32(byte[], int, int)
-     */
-    public static int hash32(final String text, final int from, final int length) {
-        return hash32(text.substring(from, from + length));
-    }
-
-    /**
      * Generates a 64-bit hash from byte array of the given length and seed.
      *
      * @param data   The input byte array
@@ -251,47 +205,4 @@ public final class MurmurHash2 {
         return hash64(data, length, 0xe17a1465);
     }
 
-    /**
-     * Generates a 64-bit hash from a string with a default seed.
-     * <p>
-     * Before 1.14 the string was converted using default encoding.
-     * Since 1.14 the string is converted to bytes using UTF-8 encoding.
-     * </p>
-     * This is a helper method that will produce the same result as:
-     *
-     * <pre>
-     * int seed = 0xe17a1465;
-     * byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
-     * int hash = MurmurHash2.hash64(bytes, bytes.length, seed);
-     * </pre>
-     *
-     * @param text The input string
-     * @return The 64-bit hash
-     * @see #hash64(byte[], int, int)
-     */
-    public static long hash64(final String text) {
-        final byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-        return hash64(bytes, bytes.length);
-    }
-
-    /**
-     * Generates a 64-bit hash from a substring with a default seed value.
-     * The string is converted to bytes using the default encoding.
-     * This is a helper method that will produce the same result as:
-     *
-     * <pre>
-     * int seed = 0xe17a1465;
-     * byte[] bytes = text.substring(from, from + length).getBytes(StandardCharsets.UTF_8);
-     * int hash = MurmurHash2.hash64(bytes, bytes.length, seed);
-     * </pre>
-     *
-     * @param text   The The input string
-     * @param from   The starting index
-     * @param length The length of the substring
-     * @return The 64-bit hash
-     * @see #hash64(byte[], int, int)
-     */
-    public static long hash64(final String text, final int from, final int length) {
-        return hash64(text.substring(from, from + length));
-    }
 }
