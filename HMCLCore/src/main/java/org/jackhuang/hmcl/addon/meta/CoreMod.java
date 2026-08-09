08f04008b49f049859b98541741b31dbbca74b6f
ToobLac <tooblac39@gmail.com>
2026-08-09 18:59:58 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/addon/meta/CoreMod.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/addon/meta/CoreMod.java
@@ -22,7 +22,6 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import kala.compress.archivers.zip.ZipArchiveEntry;
 import org.jackhuang.hmcl.addon.mod.ModLoaderType;
-import org.jackhuang.hmcl.util.Lang;
 import org.jackhuang.hmcl.util.StringUtils;
 import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.io.FileUtils;
@@ -41,7 +40,8 @@ import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 import static org.jackhuang.hmcl.util.logging.Logger.LOG;
-import static org.jackhuang.hmcl.util.versioning.GameVersionNumber.asGameVersion;
+import static org.jackhuang.hmcl.util.versioning.GameVersionNumber.atLeast;
+import static org.jackhuang.hmcl.util.versioning.GameVersionNumber.between;
 
 public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumber> mcVersionRange) {
 
@@ -49,7 +49,7 @@ public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumbe
     @Unmodifiable
     public static List<CoreMod> fromFile(Path modFile, ZipFileTree tree) {
         if (!"jar".equalsIgnoreCase(FileUtils.getExtension(modFile))) return List.of();
-        GameVersionNumber forgeMin = null, forgeMax = null, neoMin = null, neoMax = null;
+        List<CoreMod> coreMods = new ArrayList<>();
         {
             // Below 1.13
             ZipArchiveEntry mf = tree.getEntry("META-INF/MANIFEST.MF");
@@ -64,8 +64,7 @@ public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumbe
                     String fmlCorePlg = attr.getValue("FMLCorePlugin");
                     String tweakClass = attr.getValue("TweakClass");
                     if (StringUtils.isNotBlank(fmlCorePlg) || StringUtils.isNotBlank(tweakClass)) {
-                        forgeMin = asGameVersion("1.6.1");
-                        forgeMax = asGameVersion("1.12.2");
+                        coreMods.add(new CoreMod(ModLoaderType.FORGE, between("1.6.1", "1.12.2")));
                     }
                 }
             }
@@ -78,12 +77,13 @@ public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumbe
                 try (var in = new InputStreamReader(tree.getInputStream(coreModsJson))) {
                     var map = JsonUtils.fromJson(in, new TypeToken<Map<String, String>>() {
                     });
-                    if (map != null && !map.isEmpty()) {
-                        forgeMin = Lang.minNullable(forgeMin, asGameVersion("1.13"));
-                        forgeMax = asGameVersion("1.21.10"); // Removed in https://github.com/MinecraftForge/MinecraftForge/pull/10746
-                        neoMin = asGameVersion("1.20.1");
-                        neoMax = asGameVersion("1.21.4"); // Removed in https://github.com/neoforged/NeoForge/pull/2072
-                    }
+                    if (map != null && !map.isEmpty())
+                        coreMods.addAll(List.of(
+                                // Removed in https://github.com/MinecraftForge/MinecraftForge/pull/10746
+                                new CoreMod(ModLoaderType.FORGE, between("1.13", "1.21.10")),
+                                // Removed in https://github.com/neoforged/NeoForge/pull/2072
+                                new CoreMod(ModLoaderType.NEO_FORGE, between("1.20.1", "1.21.4"))
+                        ));
                 } catch (IOException | JsonIOException e) {
                     LOG.warning("Failed to read coremods.json for jar: " + modFile, e);
                 } catch (JsonSyntaxException ignored) {
@@ -92,44 +92,28 @@ public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumbe
         }
         {
             // ITransformationService
-            if (tree.getEntry("META-INF/services/cpw.mods.modlauncher.api.ITransformationService") != null) {
-                forgeMin = Lang.minNullable(forgeMin, asGameVersion("1.13"));
-                forgeMax = GameVersionNumber.unknown();
-                neoMin = asGameVersion("1.20.1");
-                neoMax = asGameVersion("1.21.8"); // Replaced by ClassProcessorProvider
-            }
+            if (tree.getEntry("META-INF/services/cpw.mods.modlauncher.api.ITransformationService") != null)
+                coreMods.addAll(List.of(
+                        new CoreMod(ModLoaderType.FORGE, atLeast("1.13.2")),
+                        new CoreMod(ModLoaderType.NEO_FORGE, between("1.20.1", "1.21.8")) // Replaced by ClassProcessorProvider
+                ));
         }
         {
             // ICoreMod for NeoForge
             // https://github.com/neoforged/FancyModLoader/pull/79
             // https://neoforged.net/news/2024-retrospection/#the-changes
             // TODO Find a sample
-            if (tree.getEntry("META-INF/services/net.neoforged.neoforgespi.coremod.ICoreMod") != null) {
-                neoMin = Lang.minNullable(neoMin, asGameVersion("1.20.5"));
-                neoMax = asGameVersion("1.21.8"); // Replaced by ClassProcessorProvider
-            }
+            if (tree.getEntry("META-INF/services/net.neoforged.neoforgespi.coremod.ICoreMod") != null)
+                coreMods.add(new CoreMod(ModLoaderType.NEO_FORGE, between("1.20.5", "1.21.8"))); // Replaced by ClassProcessorProvider
         }
         {
             // ClassProcessorProvider for NeoForge, replaces ITransformationService & ICoreMod
             // https://github.com/neoforged/NeoForge/pull/2655
             // https://github.com/neoforged/FancyModLoader/pull/358
-            if (tree.getEntry("META-INF/services/net.neoforged.neoforgespi.transformation.ClassProcessorProvider") != null) {
-                neoMin = Lang.minNullable(neoMin, asGameVersion("1.21.9"));
-                neoMax = GameVersionNumber.unknown();
-            }
+            if (tree.getEntry("META-INF/services/net.neoforged.neoforgespi.transformation.ClassProcessorProvider") != null)
+                coreMods.add(new CoreMod(ModLoaderType.NEO_FORGE, atLeast("1.21.9")));
         }
-        List<CoreMod> infoList = new ArrayList<>();
-        if (forgeMin != null && forgeMax != null)
-            infoList.add(new CoreMod(
-                    ModLoaderType.FORGE,
-                    forgeMax == GameVersionNumber.unknown() ? VersionRange.atLeast(forgeMin) : VersionRange.between(forgeMin, forgeMax)
-            ));
-        if (neoMin != null && neoMax != null)
-            infoList.add(new CoreMod(
-                    ModLoaderType.NEO_FORGE,
-                    neoMax == GameVersionNumber.unknown() ? VersionRange.atLeast(neoMin) : VersionRange.between(neoMin, neoMax)
-            ));
-        return List.copyOf(infoList);
+        return List.copyOf(coreMods);
     }
 
     @NotNull
@@ -137,8 +121,9 @@ public record CoreMod(ModLoaderType modLoaderType, VersionRange<GameVersionNumbe
     public static Set<ModLoaderType> getSupportedLoaders(List<CoreMod> coreMods, @Nullable String gameVersion) {
         Set<ModLoaderType> supportedLoaders = EnumSet.noneOf(ModLoaderType.class);
         var version = GameVersionNumber.asGameVersion(Optional.ofNullable(gameVersion));
+        if (version == GameVersionNumber.unknown()) return Set.of();
         for (var coreMod : coreMods)
-            if (coreMod.mcVersionRange().contains(Objects.requireNonNullElse(GameVersionNumber.getReleaseOfSnapshot(version), version)))
+            if (coreMod.mcVersionRange().contains(version))
                 supportedLoaders.add(coreMod.modLoaderType());
         return Set.copyOf(supportedLoaders);
     }
