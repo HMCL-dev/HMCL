a0d1fa8f38737baa6ebeac9d5439a75ef6496437
Apostrophe <Hylfrd@outlook.com>
2026-08-10 21:58:49 +0800

    修复异步任务 OOM 后界面假死并显示错误信息 (#6636)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/wizard/TaskExecutorDialogWizardDisplayer.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/wizard/TaskExecutorDialogWizardDisplayer.java
@@ -68,11 +68,22 @@ public abstract class TaskExecutorDialogWizardDisplayer extends AbstractWizardDi
                             else if (!settings.containsKey("forbid_success_message"))
                                 Controllers.dialog(i18n("message.success"), null, MessageType.SUCCESS, () -> onEnd());
                         } else {
-                            if (executor.getException() == null)
+                            if (executor.getException() == null) {
+                                onEnd();
                                 return;
+                            }
 
                             if (executor.getException() instanceof CancellationException) {
                                 onEnd();
+                                return;
+                            }
+
+                            if (executor.getException().getCause() instanceof OutOfMemoryError outOfMemoryError) {
+                                try {
+                                    Controllers.dialog(StringUtils.getStackTrace(outOfMemoryError), null, MessageType.ERROR, () -> onEnd());
+                                } catch (OutOfMemoryError ignored) {
+                                    onEnd();
+                                }
                                 return;
                             }
 
