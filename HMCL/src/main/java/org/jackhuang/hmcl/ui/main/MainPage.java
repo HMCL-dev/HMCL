ce789a1ad814fbdfb0936a965f0e633a38a93ed1
CiiLu <109708109+CiiLu@users.noreply.github.com>
2026-08-12 17:44:36 +0800

    hyw

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/main/MainPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/main/MainPage.java
@@ -380,7 +380,9 @@ public final class MainPage extends StackPane implements DecoratorPage {
                     if (exception == null) {
                         GameDirectoryManager.getSelectedRepository().setSelectedInstance(instanceHolder.value);
                         launch();
-                    } else if (!(exception instanceof CancellationException)) {
+                    } else if (exception instanceof CancellationException) {
+                        Controllers.showToast(i18n("message.cancelled"));
+                    } else {
                         LOG.warning("Failed to install game", exception);
                         Controllers.dialog(StringUtils.getStackTrace(exception),
                                 i18n("install.failed"),
