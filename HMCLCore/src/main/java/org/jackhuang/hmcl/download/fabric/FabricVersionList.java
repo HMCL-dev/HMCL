f157e6b9adfce8572ad698837da132970bd2e903
CiiLu <109708109+CiiLu@users.noreply.github.com>
2026-08-12 17:55:10 +0800

    (65) 但去尽忠 盼归诉勇

--- HMCLCore/src/main/java/org/jackhuang/hmcl/download/fabric/FabricVersionList.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/fabric/FabricVersionList.java
@@ -22,7 +22,6 @@ import org.jackhuang.hmcl.download.VersionList;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.io.NetworkUtils;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -68,39 +67,13 @@ public final class FabricVersionList extends VersionList<FabricRemoteVersion> {
     private List<String> getGameVersions(String metaUrl) throws IOException {
         String json = NetworkUtils.doGet(downloadProvider.injectURLWithCandidates(metaUrl));
         return JsonUtils.GSON.fromJson(json, listTypeOf(GameVersion.class))
-                .stream().map(GameVersion::getVersion).collect(Collectors.toList());
+                .stream().map(GameVersion::version).collect(Collectors.toList());
     }
 
     private static String getLaunchMetaUrl(String gameVersion, String loaderVersion) {
         return String.format("https://meta.fabricmc.net/v2/versions/loader/%s/%s", gameVersion, loaderVersion);
     }
 
-    private static class GameVersion {
-        private final String version;
-        private final String maven;
-        private final boolean stable;
-
-        public GameVersion() {
-            this("", null, false);
-        }
-
-        public GameVersion(String version, String maven, boolean stable) {
-            this.version = version;
-            this.maven = maven;
-            this.stable = stable;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        @Nullable
-        public String getMaven() {
-            return maven;
-        }
-
-        public boolean isStable() {
-            return stable;
-        }
+    private record GameVersion(String version, String maven, boolean stable) {
     }
 }
--- HMCLCore/src/main/java/org/jackhuang/hmcl/download/legacyfabric/LegacyFabricVersionList.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/download/legacyfabric/LegacyFabricVersionList.java
@@ -22,7 +22,6 @@ import org.jackhuang.hmcl.download.VersionList;
 import org.jackhuang.hmcl.task.Task;
 import org.jackhuang.hmcl.util.gson.JsonUtils;
 import org.jackhuang.hmcl.util.io.NetworkUtils;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -71,7 +70,7 @@ public final class LegacyFabricVersionList extends VersionList<LegacyFabricRemot
     private List<String> getGameVersions(String metaUrl) throws IOException {
         String json = NetworkUtils.doGet(downloadProvider.injectURLWithCandidates(metaUrl));
         return JsonUtils.GSON.fromJson(json, listTypeOf(GameVersion.class))
-                .stream().map(GameVersion::getVersion).collect(Collectors.toList());
+                .stream().map(GameVersion::version).collect(Collectors.toList());
     }
 
     private static String normalizeVersion(String version) {
@@ -84,32 +83,6 @@ public final class LegacyFabricVersionList extends VersionList<LegacyFabricRemot
         return String.format("https://meta.legacyfabric.net/v2/versions/loader/%s/%s", gameVersion, loaderVersion);
     }
 
-    private static class GameVersion {
-        private final String version;
-        private final String maven;
-        private final boolean stable;
-
-        public GameVersion() {
-            this("", null, false);
-        }
-
-        public GameVersion(String version, String maven, boolean stable) {
-            this.version = version;
-            this.maven = maven;
-            this.stable = stable;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        @Nullable
-        public String getMaven() {
-            return maven;
-        }
-
-        public boolean isStable() {
-            return stable;
-        }
+    private record GameVersion(String version, String maven, boolean stable) {
     }
 }
