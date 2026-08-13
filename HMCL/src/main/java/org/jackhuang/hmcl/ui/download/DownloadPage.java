ca3746319f6d51ddee2beb3f2ea7df87104151c3
辞庐 <109708109+CiiLu@users.noreply.github.com>
2026-08-13 21:39:52 +0800

    移除 “操作被取消” toast (#6658)

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/download/DownloadPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/download/DownloadPage.java
@@ -44,8 +44,8 @@ import org.jackhuang.hmcl.ui.construct.Validator;
 import org.jackhuang.hmcl.ui.decorator.DecoratorAnimatedPage;
 import org.jackhuang.hmcl.ui.decorator.DecoratorPage;
 import org.jackhuang.hmcl.ui.instances.DownloadListPage;
-import org.jackhuang.hmcl.ui.instances.HMCLLocalizedDownloadListPage;
 import org.jackhuang.hmcl.ui.instances.GameInstancePage;
+import org.jackhuang.hmcl.ui.instances.HMCLLocalizedDownloadListPage;
 import org.jackhuang.hmcl.ui.instances.Instances;
 import org.jackhuang.hmcl.ui.wizard.Navigation;
 import org.jackhuang.hmcl.ui.wizard.WizardController;
@@ -171,9 +171,7 @@ public class DownloadPage extends DecoratorAnimatedPage implements DecoratorPage
                 return task;
             }).whenComplete(Schedulers.javafx(), exception -> {
                 if (exception != null) {
-                    if (exception instanceof CancellationException) {
-                        Controllers.showToast(i18n("message.cancelled"));
-                    } else {
+                    if (!(exception instanceof CancellationException)) {
                         Controllers.dialog(DownloadProviders.localizeErrorMessage(exception), i18n("install.failed.downloading"), MessageDialogPane.MessageType.ERROR);
                     }
                 } else {
