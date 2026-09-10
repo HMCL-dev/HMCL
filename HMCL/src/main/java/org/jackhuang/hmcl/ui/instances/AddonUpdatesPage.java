f2cc90f690af2c8302a169a9ba3c1ece794e6c04
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-09-10 20:31:27 +0800

    fix import

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/instances/AddonUpdatesPage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/instances/AddonUpdatesPage.java
@@ -53,7 +53,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
