b79e25f72a04284c2098bd0137a1f74291211074
ToobLac <tooblac39@gmail.com>
2026-09-12 18:26:08 +0800

    update

--- HMCLCore/src/main/java/org/jackhuang/hmcl/game/DefaultGameInstance.java
+++ HMCLCore/src/main/java/org/jackhuang/hmcl/game/DefaultGameInstance.java
@@ -332,11 +332,13 @@ public abstract class DefaultGameInstance implements GameInstance {
     }
 
     // Uses 1.7 snapshot as there's no snapshots for 1.6 after its first release
-    private final boolean usesLegacyCoremodsDir = !getVersion().isAtLeast("1.6.1", "13w36a");
+    private @Nullable Boolean usesLegacyCoremodsDir = null;
 
     /// {@inheritDoc}
     @Override
     public @Nullable Path getCoreModsDirectory() {
+        if (usesLegacyCoremodsDir == null)
+            usesLegacyCoremodsDir = !getVersion().isAtLeast("1.6.1", "13w36a");
         return usesLegacyCoremodsDir ? getRunDirectory().resolve("coremods") : null;
     }
 
