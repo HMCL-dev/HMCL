008f783d71120d23c51008024fb85a29e79e238a
CaaMoe <aeva_d@hotmail.com>
2026-09-21 20:50:07 +0800

    还原distributionUrl, 更改服务器管理tab图标

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/instances/GameInstancePage.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/instances/GameInstancePage.java
@@ -359,7 +359,7 @@ public class GameInstancePage extends DecoratorAnimatedPage implements Decorator
                         .addNavigationDrawerTab(control.tab, control.resourcePackTab, i18n("resourcepack.manage"), SVG.TEXTURE)
                         .addNavigationDrawerTab(control.tab, control.worldListTab, i18n("world.manage"), SVG.PUBLIC)
                         .addNavigationDrawerTab(control.tab, control.schematicsTab, i18n("schematics.manage"), SVG.SCHEMA, SVG.SCHEMA_FILL)
-                        .addNavigationDrawerTab(control.tab, control.serverListTab, i18n("servers.manage"), SVG.SERVERS, SVG.SERVERS_FILL);
+                        .addNavigationDrawerTab(control.tab, control.serverListTab, i18n("servers.manage"), SVG.DRESSER);
                 VBox.setVgrow(sideBar, Priority.ALWAYS);
 
                 PopupMenu browseList = new PopupMenu();
